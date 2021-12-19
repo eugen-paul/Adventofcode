f839c4bd18ced0592b50412b5e2005d3e36497d1
Eugen Paul <paul.eugen@gmx.de>
2021-12-19 21:05:46 +0100

    Simple Junit test

--- src/main/java/net/adventofcode/y2015/day1/Day1.java
+++ src/main/java/net/adventofcode/y2015/day1/Day1.java
@@ -4,32 +4,50 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class Day1 {
 
-    private static Logger logger = null;
-
     static {
-        System.setProperty("java.util.logging.SimpleFormatter.format", "%5$s%n");
-        logger = Logger.getLogger(Day1.class.getName());
+        // must set before the Logger loads logging.properties from the classpath
+        try (InputStream is = Day1.class.getClassLoader().getResourceAsStream("logging.properties")) {
+            LogManager.getLogManager().readConfiguration(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    private static Logger logger = Logger.getLogger(Day1.class.getName());
 
     private Integer firstPositionOfbasement = null;
     private Integer onFlor = null;
 
-    public static void main(String[] args) {
-        Day1 puzzle = new Day1();
-        puzzle.doPuzzle("y2015/day1/puzzle1.txt");
+    public Integer getFirstPositionOfbasement() {
+        return this.firstPositionOfbasement;
     }
 
-    public boolean doPuzzle(String filename) {
+    public Integer getOnFlor() {
+        return this.onFlor;
+    }
+
+    public static void main(String[] args) {
+        Day1 puzzle = new Day1();
+        puzzle.doPuzzleFromFile("y2015/day1/puzzle1.txt");
+    }
+
+    public boolean doPuzzleFromFile(String filename) {
         String eventData = readDataFromFile(filename);
         if (null == eventData) {
             return false;
         }
 
+        return doPuzzleFromData(eventData);
+    }
+
+    public boolean doPuzzleFromData(String eventData) {
         if (!getFloor(0, eventData)) {
             return false;
         }
