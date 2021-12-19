8e541b5de5671fdc7b7d3a5eaef4c7f70a1d0bac
Eugen Paul <paul.eugen@gmx.de>
2021-12-19 14:03:02 +0100

    Replace System.out.println with Logger

--- src/main/java/net/adventofcode/y2015/day1/Day1.java
+++ src/main/java/net/adventofcode/y2015/day1/Day1.java
@@ -4,10 +4,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Day1 {
 
+    private static Logger logger = null;
+
+    static {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "%5$s%n");
+        logger = Logger.getLogger(Day1.class.getName());
+    }
+
     private Integer firstPositionOfbasement = null;
+    private Integer onFlor = null;
 
     public static void main(String[] args) {
         Day1 puzzle = new Day1();
@@ -20,8 +30,12 @@ public class Day1 {
             return false;
         }
 
-        System.out.println("Santa on flor " + getFloor(0, eventData));
-        System.out.println("Enter the basement on step " + firstPositionOfbasement);
+        if (!getFloor(0, eventData)) {
+            return false;
+        }
+
+        logger.log(Level.INFO, () -> "Santa on flor " + onFlor);
+        logger.log(Level.INFO, () -> "Enter the basement on step " + firstPositionOfbasement);
 
         return true;
     }
@@ -48,25 +62,25 @@ public class Day1 {
         return responseData.toString();
     }
 
-    private Integer getFloor(int startFloor, String data) {
-        int responseFlor = startFloor;
+    private boolean getFloor(int startFloor, String data) {
+        onFlor = startFloor;
         for (int i = 0; i < data.length(); i++) {
             char step = data.charAt(i);
             switch (step) {
                 case '(':
-                    responseFlor++;
+                    onFlor++;
                     break;
                 case ')':
-                    responseFlor--;
+                    onFlor--;
                     break;
                 default:
-                    return null;
+                    return false;
             }
 
-            if (null == firstPositionOfbasement && -1 == responseFlor) {
+            if (null == firstPositionOfbasement && -1 == onFlor) {
                 firstPositionOfbasement = i + 1;
             }
         }
-        return responseFlor;
+        return true;
     }
 }
