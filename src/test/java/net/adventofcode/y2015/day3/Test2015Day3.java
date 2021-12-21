ebc8aa70f589ba78efeae3cb6e203a0e121265dd
Eugen Paul <paul.eugen@gmx.de>
2021-12-21 20:45:38 +0100

    Year 2015 Day 3

---
+++ src/test/java/net/adventofcode/y2015/day3/Test2015Day3.java
@@ -0,0 +1,40 @@
+package net.adventofcode.y2015.day3;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test2015Day2
+ */
+public class Test2015Day3 {
+
+    @Test
+    public void test() {
+        testPuzzle1(">", 2);
+        testPuzzle1("^>v<", 4);
+        testPuzzle1("^v^v^v^v^v", 2);
+
+        testPuzzle2("^v", 3);
+        testPuzzle2("^>v<", 3);
+        testPuzzle2("^v^v^v^v^v", 11);
+
+        System.out.println("All tests OK.");
+    }
+
+    private void testPuzzle1(String inputData, long visitedHouseCount) {
+        Day3 event = new Day3();
+
+        assertTrue(event.doPuzzleFromData(inputData), "Testdata " + inputData);
+        assertEquals(visitedHouseCount, event.getVisitedHouseCountSantaAlone().longValue());
+    }
+
+    private void testPuzzle2(String inputData, long visitedHouseCount) {
+        Day3 event = new Day3();
+
+        assertTrue(event.doPuzzleFromData(inputData), "Testdata " + inputData);
+        assertEquals(visitedHouseCount, event.getVisitedHouseCountSantaAndRobo().longValue());
+    }
+
+}
