ee4e4b02d1c46edb2494d7d42e2788cda9cad55c
Eugen Paul <paul.eugen@gmx.de>
2021-12-22 18:32:55 +0100

    Year 2015 Day 4

---
+++ src/test/java/net/adventofcode/y2015/day4/Test2015Day4.java
@@ -0,0 +1,31 @@
+package net.adventofcode.y2015.day4;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test2015Day4
+ */
+public class Test2015Day4 {
+
+    @Test
+    public void test() throws NoSuchAlgorithmException {
+        testPuzzle("abcdef", 609043, 6742839);
+        testPuzzle("pqrstuv", 1048970, 5714438);
+
+        System.out.println("All tests OK.");
+    }
+
+    private void testPuzzle(String inputData, long lowestNumber , long totalRibbon) throws NoSuchAlgorithmException {
+        Day4 event = new Day4();
+
+        assertTrue(event.doPuzzleFromData(inputData), "Testdata " + inputData);
+        assertEquals(lowestNumber, event.getLowestFiveZeroNumber());
+        // assertEquals(totalRibbon, event.getTotalRibbon());
+    }
+
+}
