a49ea5f5c8060d52ec70ca6844f37830dd140afa
Eugen Paul <paul.eugen@gmx.de>
2022-01-04 18:19:59 +0100

    Year 2015 Day 17

---
+++ src/test/java/net/eugenpaul/adventofcode/y2015/day17/Test2015Day17.java
@@ -0,0 +1,40 @@
+package net.eugenpaul.adventofcode.y2015.day17;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test2015Day16
+ */
+class Test2015Day17 {
+
+    @Test
+    void testTest2015Day17() {
+        testPuzzle(//
+                List.of("20", //
+                        "15", //
+                        "10", //
+                        "5", //
+                        "5"//
+                ), //
+                "25", //
+                4,//
+                3//
+        );
+
+        System.out.println("All tests OK.");
+    }
+
+    private void testPuzzle(List<String> inputData, String liters, int combinations, int minCombinations) {
+        Day17 event = new Day17();
+
+        assertTrue(event.doPuzzleFromData(inputData, liters));
+        assertEquals(combinations, event.getCombinations());
+        assertEquals(minCombinations, event.getMinContainerCombinations());
+    }
+
+}
