9e162091eedf8cc29f846fc9c2e4cba176bd162d
Eugen Paul <paul.eugen@gmx.de>
2022-01-26 18:33:59 +0100

    Year 2016 Day 8

---
+++ src/main/java/net/eugenpaul/adventofcode/y2016/day8/instruction/InstructionFactory.java
@@ -0,0 +1,18 @@
+package net.eugenpaul.adventofcode.y2016.day8.instruction;
+
+public class InstructionFactory {
+    private InstructionFactory() {
+
+    }
+
+    public static Instruction fromString(String data) {
+        if (data.startsWith("rect ")) {
+            return Rect.fromString(data);
+        } else if (data.startsWith("rotate row ")) {
+            return RotateRow.fromString(data);
+        } else if (data.startsWith("rotate column ")) {
+            return RotateColumn.fromString(data);
+        }
+        return null;
+    }
+}
