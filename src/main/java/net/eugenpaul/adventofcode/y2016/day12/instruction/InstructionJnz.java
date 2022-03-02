1f75c57a51ee8f273953a43d65085e0919412e17
Eugen Paul <paul.eugen@gmx.de>
2022-03-02 19:43:51 +0100

    Year 2016 Day 23

--- src/main/java/net/eugenpaul/adventofcode/y2016/day12/instruction/InstructionJnz.java
+++ src/main/java/net/eugenpaul/adventofcode/y2016/day12/instruction/InstructionJnz.java
@@ -6,24 +6,39 @@ import net.eugenpaul.adventofcode.y2016.day12.Computer;
 @Getter
 public class InstructionJnz implements Instruction {
 
-    private int offset;
+    private Integer offset;
     private Character checkRegister;
     private Integer value;
+    private Character registerValue;
 
     public InstructionJnz(char checkRegister, int offset) {
         this.checkRegister = checkRegister;
         this.offset = offset;
         this.value = null;
+        this.registerValue = null;
     }
 
     public InstructionJnz(int value, int offset) {
         this.value = value;
         this.checkRegister = null;
         this.offset = offset;
+        this.registerValue = null;
     }
 
-    public int getOffset() {
-        return this.offset;
+    // We need this for day 23
+    public InstructionJnz(int value, char registerValue) {
+        this.value = value;
+        this.checkRegister = null;
+        this.offset = null;
+        this.registerValue = registerValue;
+    }
+
+    // We need this for day 23
+    public InstructionJnz(char checkRegister, char registerValue) {
+        this.value = null;
+        this.checkRegister = checkRegister;
+        this.offset = null;
+        this.registerValue = registerValue;
     }
 
     public static InstructionJnz fromString(String data) {
@@ -32,22 +47,41 @@ public class InstructionJnz implements Instruction {
         }
         String[] elements = data.split(" ");
 
-        int offset = Integer.parseInt(elements[2]);
-        if (elements[1].charAt(0) < '0' || elements[1].charAt(0) > '9') {
-            return new InstructionJnz(elements[1].charAt(0), Integer.parseInt(elements[2]));
+        Integer param = toNumber(elements[1]);
+        Integer offset = toNumber(elements[2]);
+        if (param != null && offset != null) {
+            return new InstructionJnz(param, offset);
         }
 
-        return new InstructionJnz(//
-                Integer.parseInt(elements[1]), //
-                offset);
+        if (param != null) {
+            return new InstructionJnz(param, elements[2].charAt(0));
+        }
+
+        if (offset != null) {
+            return new InstructionJnz(elements[1].charAt(0), offset);
+        }
+
+        return new InstructionJnz(elements[1].charAt(0), elements[2].charAt(0));
+    }
+
+    private static Integer toNumber(String input) {
+        try {
+            return Integer.parseInt(input); //
+        } catch (NumberFormatException e) {
+            return null;
+        }
     }
 
     @Override
     public void doInstruction(Computer computer) {
         if ((value != null && value != 0)//
-                || (computer.getRegister(checkRegister) != null && computer.getRegister(checkRegister) != 0)//
+                || (checkRegister != null && computer.getRegister(checkRegister) != null && computer.getRegister(checkRegister) != 0)//
         ) {
-            computer.setPosition(computer.getPosition() + offset);
+            if (offset == null) {
+                computer.setPosition(computer.getPosition() + computer.getRegister(registerValue));
+            } else {
+                computer.setPosition(computer.getPosition() + offset);
+            }
         } else {
             computer.setPosition(computer.getPosition() + 1);
         }
