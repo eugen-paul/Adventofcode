e85b8f9e8865c218981c4dce6e594701e58f1670
Eugen Paul <paul.eugen@gmx.de>
2022-04-12 19:03:25 +0200

    Year 2015 refactoring

---
+++ src/main/java/net/eugenpaul/adventofcode/helper/Crypto.java
@@ -0,0 +1,32 @@
+package net.eugenpaul.adventofcode.helper;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public final class Crypto {
+
+    private static final String MD5_LOAD_ERROR = "Cannt load MD5";
+
+    private Crypto() {
+
+    }
+
+    public static byte[] doMd5(String data) {
+        return doMd5((data).getBytes());
+    }
+
+    public static byte[] doMd5(byte[] data) {
+        return doMd5(data, 0, data.length);
+    }
+
+    public static byte[] doMd5(byte[] data, int offset, int len) {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalArgumentException(MD5_LOAD_ERROR, e);
+        }
+        md.update(data, offset, len);
+        return md.digest();
+    }
+}
