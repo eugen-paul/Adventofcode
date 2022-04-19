564bb583cf18bcf3a1b749543b64d93ba5c66ae3
Eugen Paul <paul.eugen@gmx.de>
2022-04-19 19:03:42 +0200

    Refactoring

--- src/main/java/net/eugenpaul/adventofcode/y2015/day10/Day10.java
+++ src/main/java/net/eugenpaul/adventofcode/y2015/day10/Day10.java
@@ -1,75 +1,51 @@
 package net.eugenpaul.adventofcode.y2015.day10;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
-import net.eugenpaul.adventofcode.helper.FileReaderHelper;
+import lombok.Getter;
+import lombok.Setter;
+import net.eugenpaul.adventofcode.helper.SolutionTemplate;
 
-public class Day10 {
+public class Day10 extends SolutionTemplate {
 
-    static {
-        // must set before the Logger loads logging.properties from the classpath
-        try (InputStream is = Day10.class.getClassLoader().getResourceAsStream("logging.properties")) {
-            LogManager.getLogManager().readConfiguration(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    @Setter
+    private int steps = 40;
+    @Setter
+    private int additionalSteps = 10;
 
-    private static Logger logger = Logger.getLogger(Day10.class.getName());
+    @Getter
+    private int lengthPuzzle1;
 
-    private String resultString;
-
-    public String getResult() {
-        return resultString;
-    }
+    @Getter
+    private int lengthPuzzle2;
 
     public static void main(String[] args) {
         Day10 puzzle1 = new Day10();
-        puzzle1.doPuzzleFromFile("y2015/day10/puzzle1.txt", 40);
-
-        Day10 puzzle2 = new Day10();
-        puzzle2.doPuzzleFromData(puzzle1.getResult(), 10);
+        puzzle1.doPuzzleFromFile("y2015/day10/puzzle1.txt");
     }
 
-    public boolean doPuzzleFromFile(String filename, int steps) {
-        String eventData = FileReaderHelper.readStringFromFile(filename);
-        if (null == eventData) {
-            return false;
-        }
-
-        return doPuzzleFromData(eventData, steps);
-    }
-
-    public boolean doPuzzleFromData(String eventData, int steps) {
-        if (!doEvent(eventData, steps)) {
-            return false;
-        }
-
-        logger.log(Level.INFO, () -> "resultString.length: " + getResult().length());
-
-        return true;
-    }
-
-    private boolean doEvent(String eventData, int steps) {
-        if (null == eventData) {
-            return false;
-        }
-
+    @Override
+    public boolean doEvent(String eventData) {
         String result = eventData;
+
         for (int i = 0; i < steps; i++) {
-            result = toStep(result);
+            result = doStep(result);
         }
 
-        resultString = result;
+        lengthPuzzle1 = result.length();
+
+        for (int i = 0; i < additionalSteps; i++) {
+            result = doStep(result);
+        }
+        lengthPuzzle2 = result.length();
+
+        logger.log(Level.INFO, () -> "the length of the result:     " + getLengthPuzzle1());
+        logger.log(Level.INFO, () -> "the length of the new result: " + getLengthPuzzle2());
 
         return true;
     }
 
-    public String toStep(String input) {
+    public String doStep(String input) {
         char lastDigit = input.charAt(0);
         int digitCount = 1;
 
