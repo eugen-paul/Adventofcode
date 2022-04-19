405114e12aa1fa7c3acb948786fea7c827b1977d
Eugen Paul <paul.eugen@gmx.de>
2022-04-19 19:03:42 +0200

    Refactoring

--- src/main/java/net/eugenpaul/adventofcode/y2015/day9/Day9.java
+++ src/main/java/net/eugenpaul/adventofcode/y2015/day9/Day9.java
@@ -1,28 +1,14 @@
 package net.eugenpaul.adventofcode.y2015.day9;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
-import net.eugenpaul.adventofcode.helper.FileReaderHelper;
+import net.eugenpaul.adventofcode.helper.Permutation;
+import net.eugenpaul.adventofcode.helper.SolutionTemplate;
 
-public class Day9 {
-
-    static {
-        // must set before the Logger loads logging.properties from the classpath
-        try (InputStream is = Day9.class.getClassLoader().getResourceAsStream("logging.properties")) {
-            LogManager.getLogManager().readConfiguration(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static Logger logger = Logger.getLogger(Day9.class.getName());
+public class Day9 extends SolutionTemplate {
 
     private FullMap fullMap;
     private List<Integer> distances;
@@ -44,31 +30,8 @@ public class Day9 {
         puzzle.doPuzzleFromFile("y2015/day9/puzzle1.txt");
     }
 
-    public boolean doPuzzleFromFile(String filename) {
-        List<String> eventData = FileReaderHelper.readListStringFromFile(filename);
-        if (null == eventData) {
-            return false;
-        }
-
-        return doPuzzleFromData(eventData);
-    }
-
-    public boolean doPuzzleFromData(List<String> eventData) {
-        if (!doEvent(eventData)) {
-            return false;
-        }
-
-        logger.log(Level.INFO, () -> "shortestRoteLength: " + getShortestRoteLength());
-        logger.log(Level.INFO, () -> "longestRoteLength: " + getLongestRoteLength());
-
-        return true;
-    }
-
-    private boolean doEvent(List<String> eventData) {
-        if (null == eventData) {
-            return false;
-        }
-
+    @Override
+    public boolean doEvent(List<String> eventData) {
         distances = new ArrayList<>();
 
         for (String data : eventData) {
@@ -77,39 +40,18 @@ public class Day9 {
 
         List<String> locations = fullMap.getLocationList();
 
-        permutationsRecursive(locations.size(), locations.toArray(new String[0]));
+        Permutation.permutationsRecursive(//
+                locations.size(), //
+                locations.toArray(new String[0]), //
+                this::computeDistanceOfPermutation //
+        );
 
         Collections.sort(distances);
 
+        logger.log(Level.INFO, () -> "shortestRoteLength: " + getShortestRoteLength());
+        logger.log(Level.INFO, () -> "longestRoteLength: " + getLongestRoteLength());
+
         return true;
-    }
-
-    /**
-     * Permutation with Heap's algorithm.
-     * 
-     * @param n        number of elements.
-     * @param elements input array (will be modified).
-     */
-    private void permutationsRecursive(int n, String[] elements) {
-        if (n == 1) {
-            computeDistanceOfPermutation(elements);
-        } else {
-            for (int i = 0; i < n - 1; i++) {
-                permutationsRecursive(n - 1, elements);
-                if (n % 2 == 0) {
-                    swap(elements, i, n - 1);
-                } else {
-                    swap(elements, 0, n - 1);
-                }
-            }
-            permutationsRecursive(n - 1, elements);
-        }
-    }
-
-    private void swap(String[] input, int a, int b) {
-        String tmp = input[a];
-        input[a] = input[b];
-        input[b] = tmp;
     }
 
     private void computeDistanceOfPermutation(String[] input) {
