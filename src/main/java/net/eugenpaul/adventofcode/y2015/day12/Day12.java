e1deeaee472d976cec07289c6eb4640db8a61615
Eugen Paul <paul.eugen@gmx.de>
2022-04-20 20:27:57 +0200

    Refactoring

--- src/main/java/net/eugenpaul/adventofcode/y2015/day12/Day12.java
+++ src/main/java/net/eugenpaul/adventofcode/y2015/day12/Day12.java
@@ -1,73 +1,32 @@
 package net.eugenpaul.adventofcode.y2015.day12;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-import net.eugenpaul.adventofcode.helper.FileReaderHelper;
+import lombok.Getter;
+import net.eugenpaul.adventofcode.helper.SolutionTemplate;
 
-public class Day12 {
+public class Day12 extends SolutionTemplate {
 
-    static {
-        // must set before the Logger loads logging.properties from the classpath
-        try (InputStream is = Day12.class.getClassLoader().getResourceAsStream("logging.properties")) {
-            LogManager.getLogManager().readConfiguration(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static Logger logger = Logger.getLogger(Day12.class.getName());
-
+    @Getter
     private Integer sum = null;
+    @Getter
     private Integer sumWithoutRed = null;
-
-    public Integer getSum() {
-        return sum;
-    }
-
-    public Integer getSumWithoutRed() {
-        return sumWithoutRed;
-    }
 
     public static void main(String[] args) {
         Day12 puzzle = new Day12();
         puzzle.doPuzzleFromFile("y2015/day12/puzzle1.txt");
     }
 
-    public boolean doPuzzleFromFile(String filename) {
-        String eventData = FileReaderHelper.readStringFromFile(filename);
-        if (null == eventData) {
-            return false;
-        }
-
-        return doPuzzleFromData(eventData);
-    }
-
-    public boolean doPuzzleFromData(String eventData) {
-        if (!doEvent(eventData)) {
-            return false;
-        }
-
-        logger.log(Level.INFO, () -> "sum  " + getSum());
-        logger.log(Level.INFO, () -> "sumWithoutRed  " + getSumWithoutRed());
-
-        return true;
-    }
-
-    private boolean doEvent(String eventData) {
-        if (null == eventData) {
-            return false;
-        }
-
+    @Override
+    public boolean doEvent(String eventData) {
         DocumentObject data = DocumentParser.parseData(eventData);
 
         sum = getSum(data);
         sumWithoutRed = getSumWithoutRed(data);
 
+        logger.log(Level.INFO, () -> "sum  " + getSum());
+        logger.log(Level.INFO, () -> "sumWithoutRed  " + getSumWithoutRed());
         return true;
     }
 
