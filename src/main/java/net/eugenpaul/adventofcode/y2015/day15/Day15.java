a18f0a3aea66dbe35a4c4e364121b4f31973ed6f
Eugen Paul <paul.eugen@gmx.de>
2022-04-25 19:38:33 +0200

    Refactoring

--- src/main/java/net/eugenpaul/adventofcode/y2015/day15/Day15.java
+++ src/main/java/net/eugenpaul/adventofcode/y2015/day15/Day15.java
@@ -1,91 +1,72 @@
 package net.eugenpaul.adventofcode.y2015.day15;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
-import net.eugenpaul.adventofcode.helper.FileReaderHelper;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import net.eugenpaul.adventofcode.helper.SolutionTemplate;
 
-public class Day15 {
+public class Day15 extends SolutionTemplate {
 
-    static {
-        // must set before the Logger loads logging.properties from the classpath
-        try (InputStream is = Day15.class.getClassLoader().getResourceAsStream("logging.properties")) {
-            LogManager.getLogManager().readConfiguration(is);
-        } catch (IOException e) {
-            e.printStackTrace();
+    @AllArgsConstructor
+    @Getter
+    private static class Ingredients {
+        private int capacity;
+        private int durability;
+        private int flavor;
+        private int texture;
+        private int calories;
+
+        public static Ingredients fromString(String data) {
+            // Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+            String dataWithOutComma = data.replace(",", "");
+            String[] elements = dataWithOutComma.split(" ");
+            return new Ingredients(//
+                    Integer.parseInt(elements[2]), //
+                    Integer.parseInt(elements[4]), //
+                    Integer.parseInt(elements[6]), //
+                    Integer.parseInt(elements[8]), //
+                    Integer.parseInt(elements[10]) //
+            );
         }
     }
 
-    private static Logger logger = Logger.getLogger(Day15.class.getName());
-
+    @Getter
     private long maxTotalscore;
+    @Getter
     private long maxTotalscoreWithCalorieLimit;
 
-    public long getMaxTotalscore() {
-        return maxTotalscore;
-    }
-
-    public long getMaxTotalscoreWithCalorieLimit() {
-        return maxTotalscoreWithCalorieLimit;
-    }
+    private int calorie = 500;
 
     public static void main(String[] args) {
         Day15 puzzle = new Day15();
-        puzzle.doPuzzleFromFile("y2015/day15/puzzle1.txt", "y2015/day15/puzzle1_totalCalorie.txt");
+        puzzle.doPuzzleFromFile("y2015/day15/puzzle1.txt");
     }
 
-    public boolean doPuzzleFromFile(String filename, String filenameCalorie) {
-        List<String> eventData = FileReaderHelper.readListStringFromFile(filename);
-        if (null == eventData) {
-            return false;
-        }
-
-        int calorie = Integer.parseInt(FileReaderHelper.readStringFromFile(filenameCalorie));
-
-        return doPuzzleFromData(eventData, calorie);
-    }
-
-    public boolean doPuzzleFromData(List<String> eventData, int calorie) {
-        if (!doEvent(eventData, calorie)) {
-            return false;
-        }
-
-        logger.log(Level.INFO, () -> "maxTotalscore: " + getMaxTotalscore());
-        logger.log(Level.INFO, () -> "maxTotalscoreWithCalorieLimit: " + getMaxTotalscoreWithCalorieLimit());
-
-        return true;
-    }
-
-    private boolean doEvent(List<String> eventData, int calorie) {
-        if (null == eventData) {
-            return false;
-        }
-
-        List<Recipe> recipes = new ArrayList<>();
-        for (String data : eventData) {
-            recipes.add(Recipe.fromString(data));
-        }
+    @Override
+    public boolean doEvent(List<String> eventData) {
+        List<Ingredients> recipes = eventData.stream()//
+                .map(Ingredients::fromString)//
+                .collect(Collectors.toCollection(ArrayList::new));
         int[] teaspoons = new int[recipes.size()];
 
         findBestScore(recipes, teaspoons, 100, 0, calorie);
 
+        logger.log(Level.INFO, () -> "maxTotalscore: " + getMaxTotalscore());
+        logger.log(Level.INFO, () -> "maxTotalscoreWithCalorieLimit: " + getMaxTotalscoreWithCalorieLimit());
         return true;
     }
 
-    private void findBestScore(List<Recipe> recipes, int[] teaspoons, int remainderTeaspoons, int recipePosition, int calorie) {
-        if (recipePosition == recipes.size() - 1) {
+    private void findBestScore(List<Ingredients> ingredients, int[] teaspoons, int remainderTeaspoons, int recipePosition, int calorie) {
+        if (recipePosition == ingredients.size() - 1) {
             teaspoons[recipePosition] = remainderTeaspoons;
-            long score = computescore(recipes, teaspoons);
-            if (maxTotalscore < score) {
-                maxTotalscore = score;
-            }
+            long score = computescore(ingredients, teaspoons);
+            maxTotalscore = Math.max(maxTotalscore, score);
 
-            long cookieCalorie = computeCalories(recipes, teaspoons);
+            long cookieCalorie = computeCalories(ingredients, teaspoons);
             if (cookieCalorie == calorie && maxTotalscoreWithCalorieLimit < score) {
                 maxTotalscoreWithCalorieLimit = score;
             }
@@ -94,49 +75,37 @@ public class Day15 {
 
         for (int i = remainderTeaspoons; i >= 0; i--) {
             teaspoons[recipePosition] = i;
-            findBestScore(recipes, teaspoons, remainderTeaspoons - i, recipePosition + 1, calorie);
+            findBestScore(ingredients, teaspoons, remainderTeaspoons - i, recipePosition + 1, calorie);
         }
     }
 
-    private long computescore(List<Recipe> recipes, int[] teaspoons) {
+    private long computescore(List<Ingredients> ingredients, int[] teaspoons) {
         long totalCapacity = 0l;
         long totalDurability = 0l;
         long totalFlavor = 0l;
         long totalTexture = 0l;
-        for (int i = 0; i < recipes.size(); i++) {
-            totalCapacity += recipes.get(i).getCapacity() * teaspoons[i];
-            totalDurability += recipes.get(i).getDurability() * teaspoons[i];
-            totalFlavor += recipes.get(i).getFlavor() * teaspoons[i];
-            totalTexture += recipes.get(i).getTexture() * teaspoons[i];
+        for (int i = 0; i < ingredients.size(); i++) {
+            totalCapacity += ingredients.get(i).getCapacity() * teaspoons[i];
+            totalDurability += ingredients.get(i).getDurability() * teaspoons[i];
+            totalFlavor += ingredients.get(i).getFlavor() * teaspoons[i];
+            totalTexture += ingredients.get(i).getTexture() * teaspoons[i];
         }
 
-        if (totalCapacity < 0) {
-            totalCapacity = 0;
-        }
-        if (totalDurability < 0) {
-            totalDurability = 0;
-        }
-        if (totalFlavor < 0) {
-            totalFlavor = 0;
-        }
-        if (totalTexture < 0) {
-            totalTexture = 0;
-        }
+        totalCapacity = Math.max(0, totalCapacity);
+        totalDurability = Math.max(0, totalDurability);
+        totalFlavor = Math.max(0, totalFlavor);
+        totalTexture = Math.max(0, totalTexture);
 
         return totalCapacity * totalDurability * totalFlavor * totalTexture;
     }
 
-    private long computeCalories(List<Recipe> recipes, int[] teaspoons) {
+    private long computeCalories(List<Ingredients> ingredients, int[] teaspoons) {
         long totalCalories = 0l;
-        for (int i = 0; i < recipes.size(); i++) {
-            totalCalories += recipes.get(i).getCalories() * teaspoons[i];
+        for (int i = 0; i < ingredients.size(); i++) {
+            totalCalories += ingredients.get(i).getCalories() * teaspoons[i];
         }
 
-        if (totalCalories < 0) {
-            totalCalories = 0;
-        }
-
-        return totalCalories;
+        return Math.max(0, totalCalories);
     }
 
 }
--- src/test/java/net/eugenpaul/adventofcode/y2015/day15/Test2015Day15.java
+++ src/test/java/net/eugenpaul/adventofcode/y2015/day15/Test2015Day15.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import net.eugenpaul.adventofcode.helper.FileReaderHelper;
+
 /**
  * Test2015Day15
  */
@@ -17,17 +19,27 @@ class Test2015Day15 {
         testPuzzle(List.of(//
                 "Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8", //
                 "Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3" //
-        ), 62842880, 57600000, 500);
+        ), 62842880, 57600000);
 
         System.out.println("All tests OK.");
     }
 
-    private void testPuzzle(List<String> inputData, long totalscore, long scoreWithCalorieLimit, int calorie) {
+    private void testPuzzle(List<String> inputData, long totalscore, long scoreWithCalorieLimit) {
         Day15 event = new Day15();
 
-        assertTrue(event.doPuzzleFromData(inputData, calorie));
+        assertTrue(event.doPuzzleFromData(inputData));
         assertEquals(totalscore, event.getMaxTotalscore());
         assertEquals(scoreWithCalorieLimit, event.getMaxTotalscoreWithCalorieLimit());
+    }
+
+    @Test
+    void testSolution2018Day9() {
+        Day15 event = new Day15();
+
+        List<String> eventData = FileReaderHelper.readListStringFromFile("y2015/day15/puzzle1.txt");
+        assertTrue(event.doPuzzleFromData(eventData));
+        assertEquals(18965440, event.getMaxTotalscore());
+        assertEquals(15862900, event.getMaxTotalscoreWithCalorieLimit());
     }
 
 }
