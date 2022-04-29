21bfb8ff67705b34b9ef9b10d41d2ec295f9a869
Eugen Paul <paul.eugen@gmx.de>
2022-04-29 19:41:55 +0200

    Refactoring

--- src/test/java/net/eugenpaul/adventofcode/y2015/day18/Test2015Day18.java
+++ src/test/java/net/eugenpaul/adventofcode/y2015/day18/Test2015Day18.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import net.eugenpaul.adventofcode.helper.FileReaderHelper;
+
 /**
  * Test2015Day18
  */
@@ -22,20 +24,31 @@ class Test2015Day18 {
                         "#.#..#", //
                         "####.."//
                 ), //
-                "4", //
-                4,//
+                4, //
+                4, //
                 14//
         );
 
         System.out.println("All tests OK.");
     }
 
-    private void testPuzzle(List<String> inputData, String steps, int lightsOn, int brockenLightsOn) {
+    private void testPuzzle(List<String> inputData, int steps, int lightsOn, int brockenLightsOn) {
         Day18 event = new Day18();
+        event.setSteps(steps);
 
-        assertTrue(event.doPuzzleFromData(inputData, steps));
+        assertTrue(event.doPuzzleFromData(inputData));
         assertEquals(lightsOn, event.getLightsOn());
         assertEquals(brockenLightsOn, event.getLightsBrokenOn());
+    }
+
+    @Test
+    void testSolution2015Day18() {
+        Day18 event = new Day18();
+
+        List<String> eventData = FileReaderHelper.readListStringFromFile("y2015/day18/puzzle1.txt");
+        assertTrue(event.doPuzzleFromData(eventData));
+        assertEquals(768, event.getLightsOn());
+        assertEquals(781, event.getLightsBrokenOn());
     }
 
 }
