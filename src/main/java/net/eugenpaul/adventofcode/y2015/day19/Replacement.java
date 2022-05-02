cdecf4f4e6eabe18244b889afd98826febf873e1
Eugen Paul <paul.eugen@gmx.de>
2022-05-02 15:09:48 +0200

    Refactoring

--- src/main/java/net/eugenpaul/adventofcode/y2015/day19/Replacement.java
+++ src/main/java/net/eugenpaul/adventofcode/y2015/day19/Replacement.java
@@ -1,27 +1,14 @@
 package net.eugenpaul.adventofcode.y2015.day19;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class Replacement {
 
     private String input;
     private String output;
-
-    private Replacement(String input, String output) {
-        this.input = input;
-        this.output = output;
-    }
-
-    public String getInput() {
-        return this.input;
-    }
-
-    public String getOutput() {
-        return this.output;
-    }
-
-    @Override
-    public String toString() {
-        return input + "=>" + output;
-    }
 
     public static Replacement fromString(String data) {
         String[] elements = data.split(" ");
