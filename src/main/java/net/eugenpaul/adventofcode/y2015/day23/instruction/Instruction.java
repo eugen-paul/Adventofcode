d6cccf158b505afc794ef628e09248b257d042d6
Eugen Paul <paul.eugen@gmx.de>
2022-05-10 19:24:31 +0200

    Refactoring

--- src/main/java/net/eugenpaul/adventofcode/y2015/day23/instruction/Instruction.java
+++
@@ -1,8 +0,0 @@
-package net.eugenpaul.adventofcode.y2015.day23.instruction;
-
-import net.eugenpaul.adventofcode.y2015.day23.Computer;
-
-public interface Instruction {
-    
-    public void doInstruction(Computer register);
-}
