e74476566f3c5ef2cc25fe40c4667f81cfc82751
Eugen Paul <paul.eugen@gmx.de>
2022-05-11 20:50:56 +0200

    Refactoring

--- src/main/java/net/eugenpaul/adventofcode/y2015/day24/Day24.java
+++ src/main/java/net/eugenpaul/adventofcode/y2015/day24/Day24.java
@@ -1,98 +1,66 @@
 package net.eugenpaul.adventofcode.y2015.day24;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 import java.util.Map.Entry;
+import java.util.TreeMap;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-import net.eugenpaul.adventofcode.helper.FileReaderHelper;
+import lombok.Getter;
+import net.eugenpaul.adventofcode.helper.SolutionTemplate;
 
-public class Day24 {
+public class Day24 extends SolutionTemplate {
 
-    static {
-        // must set before the Logger loads logging.properties from the classpath
-        try (InputStream is = Day24.class.getClassLoader().getResourceAsStream("logging.properties")) {
-            LogManager.getLogManager().readConfiguration(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static Logger logger = Logger.getLogger(Day24.class.getName());
-
-    private long quantumEntanglement;
-
-    public long getQuantumEntanglement() {
-        return quantumEntanglement;
-    }
+    @Getter
+    private long quantumEntanglement3;
+    @Getter
+    private long quantumEntanglement4;
 
     public static void main(String[] args) {
         Day24 puzzle = new Day24();
         puzzle.doPuzzleFromFile("y2015/day24/puzzle1.txt");
     }
 
-    public boolean doPuzzleFromFile(String filename) {
-        List<String> eventData = FileReaderHelper.readListStringFromFile(filename);
-        if (null == eventData) {
-            return false;
-        }
+    @Override
+    public boolean doEvent(List<String> eventData) {
 
-        return doPuzzleFromData(eventData);
-    }
+        quantumEntanglement3 = doPuzzle(eventData.stream().map(Integer::parseInt).collect(Collectors.toList()), 3);
 
-    public boolean doPuzzleFromData(List<String> eventData) {
-        if (!doEvent(eventData.stream().map(Integer::parseInt).collect(Collectors.toList()), 3)) {
-            logger.log(Level.INFO, () -> "Solution not found :(");
-            return false;
-        }
+        logger.log(Level.INFO, () -> "min quantumEntanglement by 3 groups : " + getQuantumEntanglement3());
 
-        logger.log(Level.INFO, () -> "min quantumEntanglement by 3 groups : " + getQuantumEntanglement());
+        quantumEntanglement4 = doPuzzle(eventData.stream().map(Integer::parseInt).collect(Collectors.toList()), 4);
 
-        if (!doEvent(eventData.stream().map(Integer::parseInt).collect(Collectors.toList()), 4)) {
-            logger.log(Level.INFO, () -> "Solution not found :(");
-            return false;
-        }
-
-        logger.log(Level.INFO, () -> "min quantumEntanglement by 4 groups : " + getQuantumEntanglement());
+        logger.log(Level.INFO, () -> "min quantumEntanglement by 4 groups : " + getQuantumEntanglement4());
 
         return true;
     }
 
-    public boolean doEvent(List<Integer> eventData, int groupCount) {
-        if (null == eventData) {
-            return false;
-        }
-
+    public long doPuzzle(List<Integer> eventData, int groupCount) {
         int groupWeight = eventData.stream().reduce(0, Integer::sum) / groupCount;
         TreeMap<Integer, List<List<Integer>>> allGroups = findGroupsWithWeight(new LinkedList<>(eventData), groupWeight);
 
         List<List<Integer>> asList = treeMapToList(allGroups);
 
         Integer minGroupLen = null;
-        quantumEntanglement = Integer.MAX_VALUE;
+        long response = Integer.MAX_VALUE;
 
         for (List<Integer> list : asList) {
             if (isOk(asList, list, groupCount)) {
                 long qe = getQuantumEntanglement(list);
                 if (minGroupLen == null) {
-                    quantumEntanglement = qe;
+                    response = qe;
                     minGroupLen = list.size();
                 } else if (minGroupLen < list.size()) {
-                    return true;
-                } else if (qe < quantumEntanglement) {
-                    quantumEntanglement = qe;
+                    return response;
+                } else if (qe < response) {
+                    response = qe;
                 }
             }
         }
 
-        return true;
+        return response;
     }
 
     /**
@@ -219,15 +187,8 @@ public class Day24 {
     }
 
     private void addGroupToTree(LinkedList<Integer> group, Map<Integer, List<List<Integer>>> response) {
-        response.compute(group.size(), (k, v) -> {
-            if (v == null) {
-                List<List<Integer>> value = new LinkedList<>();
-                value.add(new LinkedList<>(group));
-                return value;
-            }
-            v.add(new LinkedList<>(group));
-            return v;
-        });
+        response.computeIfAbsent(group.size(), k -> new LinkedList<>())//
+                .add(new LinkedList<>(group));
     }
 
     private Long getQuantumEntanglement(List<Integer> group) {
--- src/test/java/net/eugenpaul/adventofcode/y2015/day24/Test2015Day24.java
+++ src/test/java/net/eugenpaul/adventofcode/y2015/day24/Test2015Day24.java
@@ -1,10 +1,13 @@
 package net.eugenpaul.adventofcode.y2015.day24;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
+import net.eugenpaul.adventofcode.helper.FileReaderHelper;
 
 /**
  * Test2015Day24
@@ -12,19 +15,22 @@ import org.junit.jupiter.api.Test;
 class Test2015Day24 {
 
     @Test
-    void test2015Day24GroupsOf3() {
+    void test2015Day24_puzzle() {
         Day24 puzzle = new Day24();
 
-        puzzle.doEvent(List.of(1, 2, 3, 4, 5, 7, 8, 9, 10, 11), 3);
-        assertEquals(99, puzzle.getQuantumEntanglement());
+        puzzle.doEvent(List.of("1", "2", "3", "4", "5", "7", "8", "9", "10", "11"));
+        assertEquals(99, puzzle.getQuantumEntanglement3());
+        assertEquals(44, puzzle.getQuantumEntanglement4());
     }
 
     @Test
-    void test2015Day24GroupsOf4() {
-        Day24 puzzle = new Day24();
+    void testSolution2015Day24() {
+        Day24 event = new Day24();
 
-        puzzle.doEvent(List.of(1, 2, 3, 4, 5, 7, 8, 9, 10, 11), 4);
-        assertEquals(44, puzzle.getQuantumEntanglement());
+        List<String> eventData = FileReaderHelper.readListStringFromFile("y2015/day24/puzzle1.txt");
+        assertTrue(event.doPuzzleFromData(eventData));
+        assertEquals(10439961859L, event.getQuantumEntanglement3());
+        assertEquals(72050269L, event.getQuantumEntanglement4());
     }
 
 }
