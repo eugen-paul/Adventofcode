385d2ff2ee00657c095b9f5adfb791ac09bc491b
Eugen Paul <paul.eugen@gmx.de>
2022-05-14 15:47:23 +0200

    Year 2018 Day 23 (Part 2, i'm too stupid for that. I didn't make it.)

---
+++ src/main/java/net/eugenpaul/adventofcode/helper/Pos3d.java
@@ -0,0 +1,12 @@
+package net.eugenpaul.adventofcode.helper;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Pos3d {
+    private long x;
+    private long y;
+    private long z;
+}
