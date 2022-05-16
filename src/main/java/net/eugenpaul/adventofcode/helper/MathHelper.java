56afd071c0e3d9850f554031d302022e6c9c4c9c
Eugen Paul <paul.eugen@gmx.de>
2022-05-16 20:32:23 +0200

    Refactoring

--- src/main/java/net/eugenpaul/adventofcode/helper/MathHelper.java
+++ src/main/java/net/eugenpaul/adventofcode/helper/MathHelper.java
@@ -8,4 +8,20 @@ public final class MathHelper {
     public static long sum(Long from, long to) {
         return (to * to + to - (from - 1L) * (from - 1L) - (from - 1L)) / 2L;
     }
+    
+    public static long modularExponentiation(long number, long exp, long mod) {
+        String expBinary = Long.toBinaryString(exp);
+
+        long response = 1L;
+        for (char element : expBinary.toCharArray()) {
+            if (element == '0') {
+                response = (response * response) % mod;
+            } else {
+                response = (response * response) % mod;
+                response = (response * number) % mod;
+            }
+        }
+
+        return response;
+    }
 }
