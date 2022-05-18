7106db34075eb6b9907a947534a036f159edc3c0
Eugen Paul <paul.eugen@gmx.de>
2022-05-18 18:26:36 +0200

    Refactoring

--- src/main/java/net/eugenpaul/adventofcode/y2016/day1/Day1.java
+++ src/main/java/net/eugenpaul/adventofcode/y2016/day1/Day1.java
@@ -1,28 +1,15 @@
 package net.eugenpaul.adventofcode.y2016.day1;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 import lombok.Getter;
-import net.eugenpaul.adventofcode.helper.FileReaderHelper;
+import net.eugenpaul.adventofcode.helper.Direction;
+import net.eugenpaul.adventofcode.helper.SimplePos;
+import net.eugenpaul.adventofcode.helper.SolutionTemplate;
 
-public class Day1 {
-
-    static {
-        // must set before the Logger loads logging.properties from the classpath
-        try (InputStream is = Day1.class.getClassLoader().getResourceAsStream("logging.properties")) {
-            LogManager.getLogManager().readConfiguration(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static Logger logger = Logger.getLogger(Day1.class.getName());
+public class Day1 extends SolutionTemplate {
 
     @Getter
     private int distance;
@@ -34,19 +21,33 @@ public class Day1 {
         puzzle.doPuzzleFromFile("y2016/day1/puzzle1.txt");
     }
 
-    public boolean doPuzzleFromFile(String filename) {
-        String eventData = FileReaderHelper.readStringFromFile(filename);
-        if (null == eventData) {
-            return false;
+    @Override
+    public boolean doEvent(String eventData) {
+
+        String[] turns = eventData.split(",");
+
+        SimplePos pos = new SimplePos(0, 0);
+        Set<SimplePos> locations = new HashSet<>();
+        distancePuzzle2 = null;
+
+        Direction direction = Direction.N;
+
+        for (String turn : turns) {
+            int range = Integer.parseInt(turn.trim().substring(1));
+
+            if (turn.trim().charAt(0) == 'L') {
+                direction = direction.turnLeft();
+            } else {
+                direction = direction.turnRight();
+            }
+
+            for (int i = 1; i <= range; i++) {
+                pos = pos.moveNew(direction);
+                saveAndCheckLocationPoint(locations, pos);
+            }
         }
 
-        return doPuzzleFromData(eventData);
-    }
-
-    public boolean doPuzzleFromData(String eventData) {
-        if (!doEvent(eventData)) {
-            return false;
-        }
+        distance = Math.abs(pos.getX()) + Math.abs(pos.getY());
 
         logger.log(Level.INFO, () -> "distance to Easter Bunny HQ: " + getDistance());
         logger.log(Level.INFO, () -> "distance from first location you visit twice to Easter Bunny HQ: " + getDistancePuzzle2());
@@ -54,59 +55,15 @@ public class Day1 {
         return true;
     }
 
-    private boolean doEvent(String eventData) {
-        if (null == eventData) {
-            return false;
-        }
-
-        String[] turns = eventData.split(",");
-
-        int posX = 0;
-        int posY = 0;
-        Map<String, Integer> locations = new HashMap<>();
-        distancePuzzle2 = null;
-
-        Direction direction = Direction.N;
-
-        for (String turn : turns) {
-            Turn t = Turn.fromString(turn.trim());
-            int range = Integer.parseInt(turn.trim().substring(1));
-
-            direction = direction.nextDirection(t);
-
-            for (int i = 1; i <= range; i++) {
-                switch (direction) {
-                case N:
-                    posY++;
-                    break;
-                case S:
-                    posY--;
-                    break;
-                case O:
-                    posX++;
-                    break;
-                case W:
-                    posX--;
-                    break;
-                }
-                saveAndCheckLocationPoint(locations, posX, posY);
-            }
-        }
-
-        distance = Math.abs(posX) + Math.abs(posY);
-
-        return true;
-    }
-
-    private void saveAndCheckLocationPoint(Map<String, Integer> locations, int posX, int posY) {
+    private void saveAndCheckLocationPoint(Set<SimplePos> locations, SimplePos pos) {
         if (null != distancePuzzle2) {
             return;
         }
-        String locationKey = posX + ":" + posY;
-        if (locations.containsKey(locationKey)) {
-            distancePuzzle2 = Math.abs(posX) + Math.abs(posY);
+
+        if (locations.contains(pos)) {
+            distancePuzzle2 = Math.abs(pos.getX()) + Math.abs(pos.getY());
             return;
         }
-        locations.put(locationKey, 1);
+        locations.add(pos);
     }
 }
