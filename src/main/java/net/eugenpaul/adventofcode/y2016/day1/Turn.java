bc36037c1c3b64f62396f79c903499e436f7bcb8
Eugen Paul <paul.eugen@gmx.de>
2022-05-18 18:26:36 +0200

    Refactoring

--- src/main/java/net/eugenpaul/adventofcode/y2016/day1/Turn.java
+++
@@ -1,9 +0,0 @@
-package net.eugenpaul.adventofcode.y2016.day1;
-
-public enum Turn {
-    R, L;
-
-    public static Turn fromString(String data) {
-        return (data.startsWith("R")) ? R : L;
-    }
-}
--- src/test/java/net/eugenpaul/adventofcode/y2016/day2/Day2Test.java
+++ src/test/java/net/eugenpaul/adventofcode/y2016/day2/Day2Test.java
@@ -7,11 +7,13 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import net.eugenpaul.adventofcode.helper.FileReaderHelper;
+
 class Day2Test {
 
     @Test
-    void testTest2016Day2_puzzle_1() {
-        testPuzzle1(List.of(//
+    void testTest2016Day2_puzzle() {
+        testPuzzle(List.of(//
                 "ULL", //
                 "RRDDD", //
                 "LURDL", //
@@ -22,12 +24,22 @@ class Day2Test {
         );
     }
 
-    private void testPuzzle1(List<String> inputData, String code, String codeExt) {
+    private void testPuzzle(List<String> inputData, String code, String codeExt) {
         Day2 event = new Day2();
 
         assertTrue(event.doPuzzleFromData(inputData));
         assertEquals(code, event.getCode());
         assertEquals(codeExt, event.getCodeExt());
+    }
+
+    @Test
+    void testSolution2019Day2() {
+        Day2 event = new Day2();
+
+        List<String> eventData = FileReaderHelper.readListStringFromFile("y2016/day2/puzzle1.txt");
+        assertTrue(event.doPuzzleFromData(eventData));
+        assertEquals("84452", event.getCode());
+        assertEquals("D65C3", event.getCodeExt());
     }
 
 }
