13705e2699e21330fc599eab3cd779be634197a1
Eugen Paul <paul.eugen@gmx.de>
2022-05-19 18:58:05 +0200

    Refactoring

--- src/main/java/net/eugenpaul/adventofcode/y2016/day4/Day4.java
+++ src/main/java/net/eugenpaul/adventofcode/y2016/day4/Day4.java
@@ -1,28 +1,13 @@
 package net.eugenpaul.adventofcode.y2016.day4;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import lombok.Getter;
-import net.eugenpaul.adventofcode.helper.FileReaderHelper;
+import net.eugenpaul.adventofcode.helper.SolutionTemplate;
 
-public class Day4 {
-
-    static {
-        // must set before the Logger loads logging.properties from the classpath
-        try (InputStream is = Day4.class.getClassLoader().getResourceAsStream("logging.properties")) {
-            LogManager.getLogManager().readConfiguration(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static Logger logger = Logger.getLogger(Day4.class.getName());
+public class Day4 extends SolutionTemplate {
 
     @Getter
     private int sumOfTheSectorIds;
@@ -35,31 +20,8 @@ public class Day4 {
         puzzle.doPuzzleFromFile("y2016/day4/puzzle1.txt");
     }
 
-    public boolean doPuzzleFromFile(String filename) {
-        List<String> eventData = FileReaderHelper.readListStringFromFile(filename);
-        if (null == eventData) {
-            return false;
-        }
-
-        return doPuzzleFromData(eventData);
-    }
-
-    public boolean doPuzzleFromData(List<String> eventData) {
-        if (!doEvent(eventData)) {
-            return false;
-        }
-
-        logger.log(Level.INFO, () -> "sumOfTheSectorIds: " + getSumOfTheSectorIds());
-        logger.log(Level.INFO, () -> "idOfNorthPole: " + getIdOfNorthPole());
-
-        return true;
-    }
-
-    private boolean doEvent(List<String> eventData) {
-        if (null == eventData) {
-            return false;
-        }
-
+    @Override
+    public boolean doEvent(List<String> eventData) {
         sumOfTheSectorIds = 0;
         idOfNorthPole = 0;
 
@@ -75,6 +37,9 @@ public class Day4 {
                 idOfNorthPole = room.getId();
             }
         }
+
+        logger.log(Level.INFO, () -> "sumOfTheSectorIds: " + getSumOfTheSectorIds());
+        logger.log(Level.INFO, () -> "idOfNorthPole: " + getIdOfNorthPole());
 
         return true;
     }
