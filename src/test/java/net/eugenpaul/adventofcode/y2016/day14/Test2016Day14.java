c6d62a5558d3b634c7861b3d19f0eccff04f0b01
Eugen Paul <paul.eugen@gmx.de>
2022-05-30 19:12:21 +0200

    Refactoring

--- src/test/java/net/eugenpaul/adventofcode/y2016/day14/Test2016Day14.java
+++ src/test/java/net/eugenpaul/adventofcode/y2016/day14/Test2016Day14.java
@@ -1,22 +1,34 @@
 package net.eugenpaul.adventofcode.y2016.day14;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Test2016Day14
- */
+import net.eugenpaul.adventofcode.helper.FileReaderHelper;
+
 class Test2016Day14 {
 
     @Test
-    void test2016Day13() {
+    void test2016Day13_testPuzzle() {
         Day14 puzzle = new Day14();
 
         puzzle.doPuzzleFromData("abc");
 
         assertEquals(22728, puzzle.getIndex());
         assertEquals(22551, puzzle.getIndex2());
+    }
+
+    @Test
+    void testSolution2016Day14() {
+        Day14 event = new Day14();
+
+        List<String> eventData = FileReaderHelper.readListStringFromFile("y2016/day14/puzzle1.txt");
+        assertTrue(event.doPuzzleFromData(eventData));
+        assertEquals(15035, event.getIndex());
+        assertEquals(19968, event.getIndex2());
     }
 
 }
--- src/test/java/net/eugenpaul/adventofcode/y2016/day15/Day15Test.java
+++ src/test/java/net/eugenpaul/adventofcode/y2016/day15/Day15Test.java
@@ -7,11 +7,13 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import net.eugenpaul.adventofcode.helper.FileReaderHelper;
+
 class Day15Test {
 
     @Test
-    void testTest2016Day15_puzzle1() {
-        testPuzzle1(List.of(//
+    void testTest2016Day15_puzzle() {
+        testPuzzle(List.of(//
                 "Disc #1 has 5 positions; at time=0, it is at position 4.", //
                 "Disc #2 has 2 positions; at time=0, it is at position 1." //
         ), //
@@ -19,11 +21,21 @@ class Day15Test {
         );
     }
 
-    private void testPuzzle1(List<String> inputData, int time) {
+    private void testPuzzle(List<String> inputData, int time) {
         Day15 event = new Day15();
 
         assertTrue(event.doPuzzleFromData(inputData));
         assertEquals(time, event.getTime());
+    }
+
+    @Test
+    void testSolution2016Day15() {
+        Day15 event = new Day15();
+
+        List<String> eventData = FileReaderHelper.readListStringFromFile("y2016/day15/puzzle1.txt");
+        assertTrue(event.doPuzzleFromData(eventData));
+        assertEquals(203660, event.getTime());
+        assertEquals(2408135, event.getTime2());
     }
 
 }
