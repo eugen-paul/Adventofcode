49972e53e8a0b103116210aa3625215d1027e5cc
Eugen Paul <paul.eugen@gmx.de>
2022-05-30 19:12:21 +0200

    Refactoring

--- src/main/java/net/eugenpaul/adventofcode/helper/MathHelper.java
+++ src/main/java/net/eugenpaul/adventofcode/helper/MathHelper.java
@@ -8,7 +8,7 @@ public final class MathHelper {
     public static long sum(Long from, long to) {
         return (to * to + to - (from - 1L) * (from - 1L) - (from - 1L)) / 2L;
     }
-    
+
     public static long modularExponentiation(long number, long exp, long mod) {
         String expBinary = Long.toBinaryString(exp);
 
@@ -23,5 +23,31 @@ public final class MathHelper {
         }
 
         return response;
+    }
+
+    /**
+     * greatest common factor
+     * 
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int gcf(int a, int b) {
+        if (a == b || b == 0)
+            return a;
+        else
+            return gcf(b, a % b);
+
+    }
+
+    /**
+     * lowest common multiple
+     * 
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int lcm(int a, int b) {
+        return a * (b / gcf(a, b));
     }
 }
