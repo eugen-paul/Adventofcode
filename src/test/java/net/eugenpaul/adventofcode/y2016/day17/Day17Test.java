91ea5fa3abf12b8995b9266eb90d132a73b1017a
Eugen Paul <paul.eugen@gmx.de>
2022-06-01 20:44:32 +0200

    Refactoring

--- src/test/java/net/eugenpaul/adventofcode/y2016/day17/Day17Test.java
+++ src/test/java/net/eugenpaul/adventofcode/y2016/day17/Day17Test.java
@@ -3,34 +3,48 @@ package net.eugenpaul.adventofcode.y2016.day17;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import net.eugenpaul.adventofcode.helper.FileReaderHelper;
 
 class Day17Test {
 
     @Test
     void testTest2016Day17_puzzle1() {
-        testPuzzle1(//
+        testPuzzle(//
                 "ihgpwlah", //
                 "DDRRRD", //
                 370 //
         );
-        testPuzzle1(//
+        testPuzzle(//
                 "kglvqrro", //
                 "DDUDRLRRUDRD", //
                 492 //
         );
-        testPuzzle1(//
+        testPuzzle(//
                 "ulqzkmiv", //
                 "DRURDRUDDLLDLUURRDULRLDUUDDDRR", //
                 830);
     }
 
-    private void testPuzzle1(String inputData, String shortestPath, int longestPathLength) {
+    private void testPuzzle(String inputData, String shortestPath, int longestPathLength) {
         Day17 event = new Day17();
 
         assertTrue(event.doPuzzleFromData(inputData));
         assertEquals(shortestPath, event.getShortestPath());
         assertEquals(longestPathLength, event.getLongestPathLength());
+    }
+
+    @Test
+    void testSolution2016Day17() {
+        Day17 event = new Day17();
+
+        List<String> eventData = FileReaderHelper.readListStringFromFile("y2016/day17/puzzle1.txt");
+        assertTrue(event.doPuzzleFromData(eventData));
+        assertEquals("RDURRDDLRD", event.getShortestPath());
+        assertEquals(526, event.getLongestPathLength());
     }
 
 }
