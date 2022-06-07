391dabe2d037d1b6d891707a18ab46db75c6e065
Eugen Paul <paul.eugen@gmx.de>
2022-06-07 15:16:06 +0200

    Refactoring

--- src/main/java/net/eugenpaul/adventofcode/y2016/day24/Day24.java
+++ src/main/java/net/eugenpaul/adventofcode/y2016/day24/Day24.java
@@ -1,31 +1,17 @@
 package net.eugenpaul.adventofcode.y2016.day24;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 import lombok.Getter;
-import net.eugenpaul.adventofcode.helper.FileReaderHelper;
-import net.eugenpaul.adventofcode.y2016.day24.DuctLayout.Pos;
+import net.eugenpaul.adventofcode.helper.Permutation;
+import net.eugenpaul.adventofcode.helper.SimplePos;
+import net.eugenpaul.adventofcode.helper.SolutionTemplate;
 
-public class Day24 {
-
-    static {
-        // must set before the Logger loads logging.properties from the classpath
-        try (InputStream is = Day24.class.getClassLoader().getResourceAsStream("logging.properties")) {
-            LogManager.getLogManager().readConfiguration(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static Logger logger = Logger.getLogger(Day24.class.getName());
+public class Day24 extends SolutionTemplate {
 
     private int shortestRouteTemp;
 
@@ -39,34 +25,15 @@ public class Day24 {
 
     private boolean returnToStart;
 
-    private Map<Pos, Map<Pos, Integer>> cache;
+    private Map<SimplePos, Map<SimplePos, Integer>> cache;
 
     public static void main(String[] args) {
         Day24 puzzle = new Day24();
         puzzle.doPuzzleFromFile("y2016/day24/puzzle1.txt");
     }
 
-    public boolean doPuzzleFromFile(String filename) {
-        List<String> eventData = FileReaderHelper.readListStringFromFile(filename);
-        if (null == eventData) {
-            return false;
-        }
-
-        return doPuzzleFromData(eventData);
-    }
-
-    public boolean doPuzzleFromData(List<String> eventData) {
-        if (!doEvent(eventData)) {
-            return false;
-        }
-
-        logger.log(Level.INFO, () -> "shortestRoute " + getShortestRoute());
-        logger.log(Level.INFO, () -> "shortestRoute2 " + getShortestRoute2());
-
-        return true;
-    }
-
-    private boolean doEvent(List<String> eventData) {
+    @Override
+    public boolean doEvent(List<String> eventData) {
         if (null == eventData) {
             return false;
         }
@@ -80,6 +47,9 @@ public class Day24 {
 
         // Puzzle 2
         shortestRoute2 = duPuzzle(true);
+
+        logger.log(Level.INFO, () -> "shortestRoute " + getShortestRoute());
+        logger.log(Level.INFO, () -> "shortestRoute2 " + getShortestRoute2());
 
         return true;
     }
@@ -97,36 +67,8 @@ public class Day24 {
             numbers.add(i);
         }
 
-        permutationsRecursive(numbers.size(), numbers.toArray(new Integer[0]));
+        Permutation.permutationsRecursive(numbers.size(), numbers.toArray(new Integer[0]), this::computeDistanceOfPermutation);
         return shortestRouteTemp;
-    }
-
-    /**
-     * Permutation with Heap's algorithm.
-     * 
-     * @param n        number of elements.
-     * @param elements input array (will be modified).
-     */
-    private void permutationsRecursive(int n, Integer[] elements) {
-        if (n == 1) {
-            computeDistanceOfPermutation(elements);
-        } else {
-            for (int i = 0; i < n - 1; i++) {
-                permutationsRecursive(n - 1, elements);
-                if (n % 2 == 0) {
-                    swap(elements, i, n - 1);
-                } else {
-                    swap(elements, 0, n - 1);
-                }
-            }
-            permutationsRecursive(n - 1, elements);
-        }
-    }
-
-    private void swap(Integer[] input, int a, int b) {
-        Integer tmp = input[a];
-        input[a] = input[b];
-        input[b] = tmp;
     }
 
     private void computeDistanceOfPermutation(Integer[] input) {
@@ -139,7 +81,7 @@ public class Day24 {
             var fromPos = startPos;
 
             route += cache.computeIfAbsent(startPos, key -> {
-                Map<Pos, Integer> value = new HashMap<>();
+                Map<SimplePos, Integer> value = new HashMap<>();
                 value.put(endPos, pathfinding.getSteps(fromPos.getX(), fromPos.getY(), endPos.getX(), endPos.getY()));
                 return value;
             }).computeIfAbsent(endPos, key -> pathfinding.getSteps(fromPos.getX(), fromPos.getY(), endPos.getX(), endPos.getY()));
