731c252a05c990b021703d6a5240b53fe561109d
Eugen Paul <paul.eugen@gmx.de>
2022-06-25 13:48:28 +0200

    Year 2019 Day 22

--- src/main/java/net/eugenpaul/adventofcode/y2019/day22/Day22.java
+++ src/main/java/net/eugenpaul/adventofcode/y2019/day22/Day22.java
@@ -9,7 +9,6 @@ import java.util.stream.Collectors;
 
 import lombok.Getter;
 import lombok.Setter;
-import net.eugenpaul.adventofcode.helper.HashStorage;
 import net.eugenpaul.adventofcode.helper.SolutionTemplate;
 
 public class Day22 extends SolutionTemplate {
@@ -22,9 +21,17 @@ public class Day22 extends SolutionTemplate {
     @Setter
     private int cardCount = 10_007;
 
-    private Stack stack;
+    private Stack simpleStack;
 
-    private class Stack {
+    private interface Shuffle {
+        void doNewStack();
+
+        void doCut(int n);
+
+        void doDealWithIncrement(int n);
+    }
+
+    private class Stack implements Shuffle {
         private LinkedList<Integer> cards = new LinkedList<>();
 
         private Stack(int cardCount) {
@@ -33,6 +40,7 @@ public class Day22 extends SolutionTemplate {
             }
         }
 
+        @Override
         public void doNewStack() {
             LinkedList<Integer> cardsTmp = new LinkedList<>();
             Iterator<Integer> rev = cards.descendingIterator();
@@ -42,6 +50,7 @@ public class Day22 extends SolutionTemplate {
             cards = cardsTmp;
         }
 
+        @Override
         public void doCut(int n) {
             LinkedList<Integer> cardsTmp = new LinkedList<>();
             if (n > 0) {
@@ -62,6 +71,7 @@ public class Day22 extends SolutionTemplate {
             }
         }
 
+        @Override
         public void doDealWithIncrement(int n) {
             Iterator<Integer> iterator = cards.iterator();
 
@@ -92,34 +102,92 @@ public class Day22 extends SolutionTemplate {
         }
     }
 
-    private class StackSimple {
+    private class StackToTrack implements Shuffle {
         private final long cardCount;
-        private long cardToShuffle;
+        private long positionToTrack;
 
-        private StackSimple(long cardCount, long cardToShuffle) {
+        private StackToTrack(long cardCount, long positionToTrack) {
             this.cardCount = cardCount;
-            this.cardToShuffle = cardToShuffle;
+            this.positionToTrack = positionToTrack;
         }
 
+        @Override
         public void doNewStack() {
-            cardToShuffle = cardCount - cardToShuffle - 1;
+            positionToTrack = cardCount - positionToTrack - 1L;
         }
 
+        @Override
         public void doCut(int n) {
-            cardToShuffle = cardToShuffle - n;
-            if (cardToShuffle < 0) {
-                cardToShuffle += cardCount;
-            } else if (cardToShuffle >= cardCount) {
-                cardToShuffle -= cardCount;
+            positionToTrack = positionToTrack - n;
+            if (positionToTrack < 0L) {
+                positionToTrack += cardCount;
+            } else if (positionToTrack >= cardCount) {
+                positionToTrack -= cardCount;
             }
         }
 
+        @Override
         public void doDealWithIncrement(int n) {
-            cardToShuffle = (cardToShuffle * n) % cardCount;
+            positionToTrack = (Math.multiplyExact(positionToTrack, n)) % cardCount;
         }
 
-        public long getCard() {
-            return cardToShuffle;
+        public long getPosition() {
+            return positionToTrack;
+        }
+
+    }
+
+    /**
+     * All changes are linear. So the whole process can be described as a function y = a*x + b.
+     */
+    private class ShuffleFunction implements Shuffle {
+        private long a;
+        private long b;
+
+        private final long size;
+
+        private ShuffleFunction(long size) {
+            this.size = size;
+            this.a = 1L;
+            this.b = 0L;
+        }
+
+        public long getA() {
+            if (a < 0L) {
+                return a + size;
+            }
+            return a;
+        }
+
+        public long getB() {
+            if (b < 0L) {
+                return b + size;
+            }
+            return b;
+        }
+
+        @Override
+        public void doCut(int n) {
+            b -= n;
+            b = b % size;
+        }
+
+        @Override
+        public void doNewStack() {
+            a *= -1L;
+            a = a % size;
+
+            b = -b - 1L;
+            b = b % size;
+        }
+
+        @Override
+        public void doDealWithIncrement(int n) {
+            a *= n;
+            a = a % size;
+
+            b *= n;
+            b = b % size;
         }
 
     }
@@ -132,39 +200,102 @@ public class Day22 extends SolutionTemplate {
     @Override
     public boolean doEvent(List<String> eventData) {
 
-        puzzle1 = doPuzzle1(eventData);
-        // puzzle2 = doPuzzle2(eventData);
+        puzzle1 = doPuzzle1(eventData, 10_007, 2019);
+        logger.log(Level.INFO, () -> "puzzle1  : " + getPuzzle1());
+
+        puzzle2 = doPuzzle2(eventData);
 
         logger.log(Level.INFO, () -> "puzzle1  : " + getPuzzle1());
+        logger.log(Level.INFO, () -> "puzzle2  : " + getPuzzle2());
 
         return true;
     }
 
-    private long doPuzzle1(List<String> eventData) {
-        StackSimple stackSimple = new StackSimple(cardCount, 2019);
-        return doOneShuffle(eventData, stackSimple);
+    private long doPuzzle2(List<String> eventData) {
+        ShuffleFunction stackSimple = new ShuffleFunction(119315717514047L);
+        doOneShuffle(eventData, stackSimple);
+
+        // Large numbers are expected here. Long is too short for this.
+        // Here you should either use a library that can calculate with very large numbers or the Windows calculator.
+        // I have decided for the second one.
+
+        // compute a and b
+        logger.log(Level.INFO, () -> "a  : " + stackSimple.getA());
+        logger.log(Level.INFO, () -> "b  : " + stackSimple.getB());
+
+        // compute x manually
+        // Function for 1 shuffle:
+        // y = a * x + b mod m
+        // Function for 2 shuffle:
+        // y = a * (a * x + b) + b mod m
+        // y = a * a * x + a * b + b mod m
+        // y = a^2 * x + b * a + b mod m
+        // Function for 3 shuffle:
+        // y = a * (a^2 * x + b * a + b) + b mod m
+        // y = a^3 * x + b * a^2 + b * a + b mod m
+        // y = a^3 * x + b * SUM{1,2}(a^i) + b mod m
+        // Function for n shuffle:
+        // y = a^n * x + b * SUM{1,n-1}(a^i) + b mod m
+        // SUM{1,n-1}(a^i) = a * (a^(n-1) - 1) / (a - 1)
+        // y = a^n * x + b * a * (a^(n-1) - 1) / (a - 1) + b mod m
+        //
+        // n = 101741582076661
+        // m = 119315717514047
+        // a = -15681174147849 = 103634543366198
+        // b = -57304980694001 = 62010736820046
+
+        // ==> y = a^n * x + b * a * (a^(n-1) - 1) / (a - 1) + b mod m
+        // ==> 2020 = 103634543366198^101741582076661 * x + 62010736820046*103634543366198*(103634543366198^(101741582076661-1)-1/103634543366198-1) +
+        // 62010736820046
+        // ==> 2020 = 70839959544740 * x + 98652742534150*(6350215898474-1/103634543366197) + 62010736820046
+        // ==> 2020 = 70839959544740 * x + 98652742534150*6350215898473/103634543366197 + 62010736820046
+        // ==> 2020 = 70839959544740 * x + 44529272864522/103634543366197 + 62010736820046
+        // ==> 2020 = 70839959544740 * x + 44529272864522*103634543366197^(m-2) + 62010736820046
+        // ==> 2020 = 70839959544740 * x + 44529272864522*103634543366197^119315717514045 + 62010736820046
+        // ==> 2020 = 70839959544740 * x + 44529272864522*76843802954738 + 62010736820046
+        // ==> 2020 = 70839959544740 * x + 69356709893769 + 62010736820046
+        // ==> 2020 = 70839959544740 * x + 12051729199768
+        // ==> 107263988316299 = 70839959544740 * x
+        // ==> 107263988316299/70839959544740 = x
+        // ==> 107263988316299*70839959544740^(m-2) = x
+        // ==> 107263988316299*70839959544740^119315717514045 = x
+        // ==> 107263988316299*87505940779834 = x
+        // ==> 93.750.418.158.025 = x
+        // ==> 93750418158025 = x
+
+        logger.log(Level.INFO, () -> "compute x manually  : 93750418158025");
+
+        return 93750418158025L;
     }
 
-    // private long doPuzzle2(List<String> eventData) {
-    //     long shuffles = 101741582076661L;
+    private long doPuzzle1(List<String> eventData, long cardCount, long card) {
+        ShuffleFunction stackSimple = new ShuffleFunction(cardCount);
+        doOneShuffle(eventData, stackSimple);
 
-    //     StackSimple stackSimple = new StackSimple(119315717514047L, 2020);
+        long response = stackSimple.getA() * card + stackSimple.getB();
+        response = response % cardCount;
+        if (response < 0) {
+            response += cardCount;
+        }
 
-    //     HashStorage st = new HashStorage();
-    //     long step = 0;
-    //     while (!st.add("" + doOneShuffle(eventData, stackSimple), step)) {
-    //         step++;
-    //         if (step % 500 == 0) {
-    //             System.out.println(step);
-    //         }
-    //     }
+        StackToTrack stackToCheck = new StackToTrack(cardCount, card);
+        doOneShuffle(eventData, stackToCheck);
 
-    //     System.out.println(st.getHashOf(101741582076661L));
-    //     return -1;
-    // }
+        if (response != stackToCheck.getPosition()) {
+            throw new IllegalStateException("ERROR");
+        }
+        return response;
+    }
 
+    /** First (slow) implementation. Just for tests. */
     public boolean testSlow(List<String> eventData) {
-        stack = new Stack(cardCount);
+        simpleStack = new Stack(cardCount);
+        doOneShuffle(eventData, simpleStack);
+
+        return true;
+    }
+
+    private void doOneShuffle(List<String> eventData, Shuffle stack) {
         for (String data : eventData) {
             if (data.startsWith("cut ")) {
                 stack.doCut(Integer.parseInt(data.substring(4)));
@@ -174,30 +305,14 @@ public class Day22 extends SolutionTemplate {
                 stack.doNewStack();
             }
         }
-
-        return true;
-    }
-
-    private long doOneShuffle(List<String> eventData, StackSimple stackSimple) {
-        for (String data : eventData) {
-            if (data.startsWith("cut ")) {
-                stackSimple.doCut(Integer.parseInt(data.substring(4)));
-            } else if (data.startsWith("deal with increment ")) {
-                stackSimple.doDealWithIncrement(Integer.parseInt(data.substring(20)));
-            } else if (data.startsWith("deal into new stack")) {
-                stackSimple.doNewStack();
-            }
-        }
-
-        return stackSimple.getCard();
     }
 
     public int getCart(int n) {
-        return stack.getCard(n);
+        return simpleStack.getCard(n);
     }
 
     public int getPos(int n) {
-        return stack.getPositionOfCard(n);
+        return simpleStack.getPositionOfCard(n);
     }
 
 }
--- src/test/java/net/eugenpaul/adventofcode/y2019/day22/Day22Test.java
+++ src/test/java/net/eugenpaul/adventofcode/y2019/day22/Day22Test.java
@@ -13,35 +13,35 @@ class Day22Test {
 
     @Test
     void testTest2019Day22_testNewStack() {
-        testPuzzle1(List.of(//
+        testShuffle(List.of(//
                 "deal into new stack" //
         ), List.of(9, 8, 7, 6, 5, 4, 3, 2, 1, 0));
     }
 
     @Test
     void testTest2019Day22_testCut1() {
-        testPuzzle1(List.of(//
+        testShuffle(List.of(//
                 "cut 2" //
         ), List.of(2, 3, 4, 5, 6, 7, 8, 9, 0, 1));
     }
 
     @Test
     void testTest2019Day22_testCut2() {
-        testPuzzle1(List.of(//
+        testShuffle(List.of(//
                 "cut -2" //
         ), List.of(8, 9, 0, 1, 2, 3, 4, 5, 6, 7));
     }
 
     @Test
     void testTest2019Day22_testIncrement() {
-        testPuzzle1(List.of(//
+        testShuffle(List.of(//
                 "deal with increment 3" //
         ), List.of(0, 7, 4, 1, 8, 5, 2, 9, 6, 3));
     }
 
     @Test
     void testTest2019Day22_testPuzzle_1() {
-        testPuzzle1(List.of(//
+        testShuffle(List.of(//
                 "deal with increment 7", //
                 "deal into new stack", //
                 "deal into new stack" //
@@ -50,7 +50,7 @@ class Day22Test {
 
     @Test
     void testTest2019Day22_testPuzzle_2() {
-        testPuzzle1(List.of(//
+        testShuffle(List.of(//
                 "cut 6", //
                 "deal with increment 7", //
                 "deal into new stack" //
@@ -59,7 +59,7 @@ class Day22Test {
 
     @Test
     void testTest2019Day22_testPuzzle_3() {
-        testPuzzle1(List.of(//
+        testShuffle(List.of(//
                 "deal with increment 7", //
                 "deal with increment 9", //
                 "cut -2" //
@@ -68,7 +68,7 @@ class Day22Test {
 
     @Test
     void testTest2019Day22_testPuzzle_4() {
-        testPuzzle1(List.of(//
+        testShuffle(List.of(//
                 "deal into new stack", //
                 "cut -2", //
                 "deal with increment 7", //
@@ -82,7 +82,7 @@ class Day22Test {
         ), List.of(9, 2, 5, 8, 1, 4, 7, 0, 3, 6));
     }
 
-    private void testPuzzle1(List<String> inputData, List<Integer> carts) {
+    private void testShuffle(List<String> inputData, List<Integer> carts) {
         Day22 event = new Day22();
 
         event.setCardCount(10);
@@ -94,14 +94,14 @@ class Day22Test {
         }
     }
 
-    // @Test
-    // void testSolution2019Day22() {
-    // Day22 event = new Day22();
+    @Test
+    void testSolution2019Day22() {
+        Day22 event = new Day22();
 
-    // List<String> eventData = FileReaderHelper.readListStringFromFile("y2019/day22/puzzle1.txt");
-    // assertTrue(event.doPuzzleFromData(eventData));
-    // assertEquals(568, event.getWayAZ());
-    // assertEquals(6546, event.getWayAZ2());
-    // }
+        List<String> eventData = FileReaderHelper.readListStringFromFile("y2019/day22/puzzle1.txt");
+        assertTrue(event.doPuzzleFromData(eventData));
+        assertEquals(6696, event.getPuzzle1());
+        assertEquals(93750418158025L, event.getPuzzle2());
+    }
 
 }
