197b20b2a05478ef35a05ac93bf52bd9c6a0de59
Eugen Paul <paul.eugen@gmx.de>
2022-08-04 19:23:33 +0200

    Year 2020 Day 24

--- src/main/java/net/eugenpaul/adventofcode/y2017/day11/Day11.java
+++ src/main/java/net/eugenpaul/adventofcode/y2017/day11/Day11.java
@@ -2,21 +2,12 @@ package net.eugenpaul.adventofcode.y2017.day11;
 
 import java.util.logging.Level;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import net.eugenpaul.adventofcode.helper.HexagonalMapV;
+import net.eugenpaul.adventofcode.helper.SimplePos;
 import net.eugenpaul.adventofcode.helper.SolutionTemplate;
 
 public class Day11 extends SolutionTemplate {
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    private class Pos {
-        private int x;
-        private int y;
-    }
 
     @Getter
     private Integer distance;
@@ -36,12 +27,12 @@ public class Day11 extends SolutionTemplate {
 
         String[] path = eventData.split(",");
 
-        Pos currentPosition = new Pos(0, 0);
+        SimplePos currentPosition = new SimplePos(0, 0);
         maxDistance = 0;
 
         for (String direction : path) {
-            computeNextPosition(currentPosition, direction);
-            distance = computeDistance(currentPosition);
+            currentPosition = HexagonalMapV.computeNextPosition(currentPosition, direction);
+            distance = HexagonalMapV.computeDistanceToZero(currentPosition);
             maxDistance = Math.max(maxDistance, distance);
         }
 
@@ -49,56 +40,6 @@ public class Day11 extends SolutionTemplate {
         logger.log(Level.INFO, () -> "maxDistance : " + getMaxDistance());
 
         return true;
-    }
-
-    private void computeNextPosition(Pos position, String direction) {
-        switch (direction) {
-        case "n":
-            position.setY(position.getY() + 1);
-            break;
-        case "ne":
-            position.setX(position.getX() + 1);
-            if ((position.getX() % 2) == 1 || (position.getX() % 2) == -1) {
-                position.setY(position.getY() + 1);
-            }
-            break;
-        case "nw":
-            position.setX(position.getX() - 1);
-            if ((position.getX() % 2) == 1 || (position.getX() % 2) == -1) {
-                position.setY(position.getY() + 1);
-            }
-            break;
-        case "se":
-            position.setX(position.getX() + 1);
-            if ((position.getX() % 2) == 0) {
-                position.setY(position.getY() - 1);
-            }
-            break;
-        case "sw":
-            position.setX(position.getX() - 1);
-            if ((position.getX() % 2) == 0) {
-                position.setY(position.getY() - 1);
-            }
-            break;
-        case "s":
-            position.setY(position.getY() - 1);
-            break;
-        default:
-            break;
-        }
-    }
-
-    private int computeDistance(Pos position) {
-        int x = Math.abs(position.getX());
-        int y = Math.abs(position.getY());
-        int cSteps = 0;
-
-        if (position.getY() < 0) {
-            cSteps = x + Math.max(0, y - x + (x + 1) / 2);
-        } else {
-            cSteps = x + Math.max(0, y - x + x / 2);
-        }
-        return cSteps;
     }
 
 }
