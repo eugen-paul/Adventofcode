a07bff49ee3a840616869a861b6b6c3d1447d239
Eugen Paul <paul.eugen@gmx.de>
2022-08-18 18:24:44 +0200

    Year 2021 Day 9

---
+++ src/test/java/net/eugenpaul/adventofcode/y2021/day9/Day9Test.java
@@ -0,0 +1,42 @@
+package net.eugenpaul.adventofcode.y2021.day9;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import net.eugenpaul.adventofcode.helper.FileReaderHelper;
+
+class Day9Test {
+
+    @Test
+    void testTest2021Day9() {
+        testPuzzle(List.of(//
+                "2199943210", //
+                "3987894921", //
+                "9856789892", //
+                "8767896789", //
+                "9899965678" //
+        ), 15, 1134);
+    }
+
+    private void testPuzzle(List<String> inputData, long sum, long mult) {
+        Day9 event = new Day9();
+
+        assertTrue(event.doPuzzleFromData(inputData));
+        assertEquals(sum, event.getSum());
+        assertEquals(mult, event.getMult());
+    }
+
+    @Test
+    void testSolution2021Day9() {
+        Day9 event = new Day9();
+
+        List<String> eventData = FileReaderHelper.readListStringFromFile("y2021/day9/puzzle1.txt");
+        assertTrue(event.doPuzzleFromData(eventData));
+        assertEquals(495, event.getSum());
+    }
+
+}
