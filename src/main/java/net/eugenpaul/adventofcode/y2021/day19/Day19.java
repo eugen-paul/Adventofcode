be3cb4cd97e073f474b08022dc73e8a4b7a3ba26
Eugen Paul <paul.eugen@gmx.de>
2022-08-26 11:25:20 +0200

    switch from list to set

--- src/main/java/net/eugenpaul/adventofcode/y2021/day19/Day19.java
+++ src/main/java/net/eugenpaul/adventofcode/y2021/day19/Day19.java
@@ -2,9 +2,11 @@ package net.eugenpaul.adventofcode.y2021.day19;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.ToLongFunction;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
@@ -17,12 +19,12 @@ public class Day19 extends SolutionTemplate {
 
     private class Scanner {
         private int number;
-        private List<Pos3d> beacons;
-        private List<Pos3d> scanners;
+        private Set<Pos3d> beacons;
+        private Set<Pos3d> scanners;
 
         public Scanner() {
-            beacons = new LinkedList<>();
-            scanners = new LinkedList<>();
+            beacons = new HashSet<>();
+            scanners = new HashSet<>();
         }
     }
 
@@ -46,9 +48,10 @@ public class Day19 extends SolutionTemplate {
         beaconscount = complete.beacons.size();
 
         maxDistance = Long.MIN_VALUE;
-        for (int a = 0; a < complete.scanners.size(); a++) {
-            for (int b = a + 1; b < complete.scanners.size(); b++) {
-                maxDistance = Math.max(maxDistance, complete.scanners.get(a).manhattanDistance(complete.scanners.get(b)));
+
+        for (var setA : complete.scanners) {
+            for (var setB : complete.scanners) {
+                maxDistance = Math.max(maxDistance, setA.manhattanDistance(setB));
             }
         }
 
@@ -80,8 +83,8 @@ public class Day19 extends SolutionTemplate {
     }
 
     private Scanner merge(Scanner a, Scanner b) {
-        List<Pos3d> aBeacons = a.beacons;
-        List<List<Pos3d>> bBeacons = getAllOrientations(b.beacons);
+        Set<Pos3d> aBeacons = a.beacons;
+        Set<Set<Pos3d>> bBeacons = getAllOrientations(b.beacons);
         List<Pos3d> deltaResponse = new LinkedList<>();
 
         for (var bVar : bBeacons) {
@@ -91,11 +94,11 @@ public class Day19 extends SolutionTemplate {
                 response.number = a.number;
                 response.beacons = m;
 
-                response.scanners = new LinkedList<>(a.scanners);
+                response.scanners = new HashSet<>(a.scanners);
                 response.scanners.addAll(//
                         b.scanners.stream()//
                                 .map(v -> v.addNew(deltaResponse.get(0))) //
-                                .collect(Collectors.toList())//
+                                .collect(Collectors.toSet())//
                 );
 
                 return response;
@@ -105,7 +108,7 @@ public class Day19 extends SolutionTemplate {
         return null;
     }
 
-    private List<Pos3d> merge(List<Pos3d> a, List<Pos3d> b, List<Pos3d> deltaResponse) {
+    private Set<Pos3d> merge(Set<Pos3d> a, Set<Pos3d> b, List<Pos3d> deltaResponse) {
 
         for (Pos3d aRef : a) {
             for (Pos3d bRef : b) {
@@ -113,16 +116,16 @@ public class Day19 extends SolutionTemplate {
                 long deltaY = aRef.getY() - bRef.getY();
                 long deltaZ = aRef.getZ() - bRef.getZ();
 
-                List<Pos3d> bDelta = b.stream()//
+                Set<Pos3d> bDelta = b.stream()//
                         .map(v -> new Pos3d(v.getX() + deltaX, v.getY() + deltaY, v.getZ() + deltaZ))//
-                        .collect(Collectors.toList());
+                        .collect(Collectors.toSet());
 
-                List<Pos3d> overlapps = bDelta.stream()//
+                Set<Pos3d> overlapps = bDelta.stream()//
                         .filter(a::contains)//
-                        .collect(Collectors.toList());
+                        .collect(Collectors.toSet());
 
                 if (overlapps.size() >= 12) {
-                    List<Pos3d> response = new LinkedList<>(a);
+                    Set<Pos3d> response = new HashSet<>(a);
                     response.addAll(bDelta.stream()//
                             .filter(v -> !a.contains(v))//
                             .collect(Collectors.toList()));
@@ -133,12 +136,12 @@ public class Day19 extends SolutionTemplate {
             }
         }
 
-        return Collections.emptyList();
+        return Collections.emptySet();
     }
 
-    private List<List<Pos3d>> getAllOrientations(List<Pos3d> beacons) {
+    private Set<Set<Pos3d>> getAllOrientations(Set<Pos3d> beacons) {
 
-        List<List<Pos3d>> response = new LinkedList<>();
+        Set<Set<Pos3d>> response = new HashSet<>();
 
         response.addAll(getAllDirection(beacons, Pos3d::getX, Pos3d::getY, Pos3d::getZ));
         response.addAll(getAllDirection(beacons, Pos3d::getX, Pos3d::getZ, Pos3d::getY));
@@ -150,13 +153,13 @@ public class Day19 extends SolutionTemplate {
         return response;
     }
 
-    private List<List<Pos3d>> getAllDirection(List<Pos3d> beacons, ToLongFunction<Pos3d> x, ToLongFunction<Pos3d> y, ToLongFunction<Pos3d> z) {
-        List<List<Pos3d>> response = new LinkedList<>();
+    private Set<Set<Pos3d>> getAllDirection(Set<Pos3d> beacons, ToLongFunction<Pos3d> x, ToLongFunction<Pos3d> y, ToLongFunction<Pos3d> z) {
+        Set<Set<Pos3d>> response = new HashSet<>();
 
         for (long xDir = -1; xDir < 2; xDir = xDir + 2) {
             for (long yDir = -1; yDir < 2; yDir = yDir + 2) {
                 for (long zDir = -1; zDir < 2; zDir = zDir + 2) {
-                    List<Pos3d> changed = new LinkedList<>();
+                    Set<Pos3d> changed = new HashSet<>();
                     for (var pos : beacons) {
                         changed.add(new Pos3d(x.applyAsLong(pos) * xDir, y.applyAsLong(pos) * yDir, z.applyAsLong(pos) * zDir));
                     }
