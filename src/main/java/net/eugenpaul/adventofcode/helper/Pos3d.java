3098d3abbfa8ceaf56d5e95d54c323f9e2ff9e1c
Eugen Paul <paul.eugen@gmx.de>
2022-12-18 13:19:21 +0100

    Some refactoring

--- src/main/java/net/eugenpaul/adventofcode/helper/Pos3d.java
+++ src/main/java/net/eugenpaul/adventofcode/helper/Pos3d.java
@@ -1,14 +1,17 @@
 package net.eugenpaul.adventofcode.helper;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import net.eugenpaul.adventofcode.helper.dijkstra.DijkstraStepGen;
 
 @AllArgsConstructor
 @Data
-public class Pos3d {
+public class Pos3d implements DijkstraStepGen {
     private long x;
     private long y;
     private long z;
@@ -57,5 +60,58 @@ public class Pos3d {
 
     public long manhattanDistance(Pos3d b) {
         return Math.abs(x - b.x) + Math.abs(y - b.y) + Math.abs(z - b.z);
+    }
+
+    @Override
+    public String compHash() {
+        return x + ";" + ";" + z;
+    }
+
+    @Override
+    public int getCost() {
+        return 1;
+    }
+
+    public List<Pos3d> getNeighbors() {
+        List<Pos3d> response = new LinkedList<>();
+        response.add(new Pos3d(x, y + 1, z));
+        response.add(new Pos3d(x, y - 1, z));
+        response.add(new Pos3d(x, y, z + 1));
+        response.add(new Pos3d(x, y, z - 1));
+        response.add(new Pos3d(x + 1, y, z));
+        response.add(new Pos3d(x - 1, y, z));
+        return response;
+    }
+
+    public List<Pos3d> getNeighborsWithDiagonal() {
+        List<Pos3d> response = new LinkedList<>();
+        response.add(new Pos3d(x, y + 1, z));
+        response.add(new Pos3d(x, y, z + 1));
+        response.add(new Pos3d(x, y + 1, z + 1));
+        response.add(new Pos3d(x, y - 1, z));
+        response.add(new Pos3d(x, y, z - 1));
+        response.add(new Pos3d(x, y - 1, z - 1));
+        response.add(new Pos3d(x, y - 1, z + 1));
+        response.add(new Pos3d(x, y + 1, z - 1));
+
+        response.add(new Pos3d(x + 1, y + 1, z));
+        response.add(new Pos3d(x + 1, y, z + 1));
+        response.add(new Pos3d(x + 1, y + 1, z + 1));
+        response.add(new Pos3d(x + 1, y - 1, z));
+        response.add(new Pos3d(x + 1, y, z - 1));
+        response.add(new Pos3d(x + 1, y - 1, z - 1));
+        response.add(new Pos3d(x + 1, y - 1, z + 1));
+        response.add(new Pos3d(x + 1, y + 1, z - 1));
+
+        response.add(new Pos3d(x - 1, y + 1, z));
+        response.add(new Pos3d(x - 1, y, z + 1));
+        response.add(new Pos3d(x - 1, y + 1, z + 1));
+        response.add(new Pos3d(x - 1, y - 1, z));
+        response.add(new Pos3d(x - 1, y, z - 1));
+        response.add(new Pos3d(x - 1, y - 1, z - 1));
+        response.add(new Pos3d(x - 1, y - 1, z + 1));
+        response.add(new Pos3d(x - 1, y + 1, z - 1));
+
+        return response;
     }
 }
