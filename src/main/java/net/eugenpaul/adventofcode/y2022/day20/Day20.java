d5ce0cea7d54d5b1fcffab61fdda2bd7edde4e82
Eugen Paul <paul.eugen@gmx.de>
2022-12-20 18:20:50 +0100

    Some refactoring + alternative solution

--- src/main/java/net/eugenpaul/adventofcode/y2022/day20/Day20.java
+++ src/main/java/net/eugenpaul/adventofcode/y2022/day20/Day20.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.eugenpaul.adventofcode.helper.CircleMemory;
 import net.eugenpaul.adventofcode.helper.SolutionTemplate;
@@ -23,20 +24,8 @@ public class Day20 extends SolutionTemplate {
     @Override
     public boolean doEvent(List<String> eventData) {
 
-        CircleMemory<Long> mem = new CircleMemory<>();
-        List<CircleMemory<Long>.CirclePosition> pos = new LinkedList<>();
-        for (String data : eventData) {
-            pos.add(mem.addLast(Long.parseLong(data)));
-        }
-
-        part1 = doPuzzle(mem, pos, 1);
-
-        mem = new CircleMemory<>();
-        pos = new LinkedList<>();
-        for (String data : eventData) {
-            pos.add(mem.addLast(Long.parseLong(data) * 811589153L));
-        }
-        part2 = doPuzzle(mem, pos, 10);
+        part1 = doPuzzleAlt(eventData, 1, 1);
+        part2 = doPuzzle(eventData, 10, 811589153);
 
         logger.log(Level.INFO, () -> "part1 : " + getPart1());
         logger.log(Level.INFO, () -> "part2 : " + getPart2());
@@ -44,24 +33,19 @@ public class Day20 extends SolutionTemplate {
         return true;
     }
 
-    private long doPuzzle(CircleMemory<Long> mem, List<CircleMemory<Long>.CirclePosition> pos, int rounds) {
+    private long doPuzzle(List<String> eventData, int rounds, int mult) {
+
+        CircleMemory<Long> mem = new CircleMemory<>();
+        List<CircleMemory<Long>.CirclePosition> pos = new LinkedList<>();
+        for (String data : eventData) {
+            pos.add(mem.addLast(Long.parseLong(data) * mult));
+        }
+
         for (int i = 0; i < rounds; i++) {
-            for (var p : pos) {
-                CircleMemory<Long>.CirclePosition mp = p;
-                long moveSteps = mp.getData();
-                while (moveSteps < 0) {
-                    moveSteps = moveSteps % (pos.size() - 1);
-                    moveSteps += pos.size() - 1;
-                }
-                while (moveSteps >= pos.size()) {
-                    moveSteps = moveSteps % (pos.size() - 1);
-                }
-                if (moveSteps == 0) {
-                    continue;
-                }
-                CircleMemory<Long>.CirclePosition n = mem.moveNext(mp, (int) moveSteps);
-                mem.removeAndMoveNext(mp);
-                mem.add(p, n);
+            for (var currentElement : pos) {
+                var prev = mem.removeAndGetPrev(currentElement);
+                var newPos = mem.moveNext(prev, (int) (currentElement.getData() % (pos.size() - 1)));
+                mem.add(currentElement, newPos);
             }
         }
 
@@ -76,5 +60,72 @@ public class Day20 extends SolutionTemplate {
         }
 
         throw new IllegalArgumentException();
+    }
+
+    @AllArgsConstructor
+    private class Data {
+        private long value;
+    }
+
+    private long doPuzzleAlt(List<String> eventData, int rounds, int mult) {
+
+        List<Data> mem = new LinkedList<>();
+        List<Data> order = new LinkedList<>();
+        for (String data : eventData) {
+            var dataEl = new Data(Long.parseLong(data) * mult);
+            mem.add(dataEl);
+            order.add(dataEl);
+        }
+
+        for (int i = 0; i < rounds; i++) {
+            doRound(mem, order);
+        }
+
+        int zeroPos = getZeroPos(mem);
+
+        int pos1000 = (zeroPos + 1000) % mem.size();
+        int pos2000 = (zeroPos + 2000) % mem.size();
+        int pos3000 = (zeroPos + 3000) % mem.size();
+
+        return mem.get(pos1000).value + mem.get(pos2000).value + mem.get(pos3000).value;
+    }
+
+    private void doRound(List<Data> mem, List<Data> order) {
+        for (Data data : order) {
+            int pos = 0;
+            Data currentEl = null;
+            var it = mem.iterator();
+            while (it.hasNext()) {
+                var el = it.next();
+                if (el == data) {
+                    currentEl = el;
+                    it.remove();
+                    break;
+                }
+                pos++;
+            }
+
+            if (currentEl == null) {
+                throw new IllegalArgumentException();
+            }
+
+            int newPos = (int) ((currentEl.value + pos) % mem.size());
+            if (newPos < 0) {
+                newPos += mem.size();
+            }
+
+            mem.add(newPos, currentEl);
+        }
+    }
+
+    private int getZeroPos(List<Data> mem) {
+        int zeroPos = 0;
+        for (Data data : mem) {
+            if (data.value == 0) {
+                break;
+            }
+            zeroPos++;
+        }
+        return zeroPos;
     }
 }
