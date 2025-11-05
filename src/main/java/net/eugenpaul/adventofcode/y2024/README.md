e408faa9480333e3f7a9b877851deaacb8db2483
Eugen Paul <paul.eugen@gmx.de>
2025-11-05 16:00:14 +0100

    Advent of Code 2024 Day 17

--- src/main/java/net/eugenpaul/adventofcode/y2024/README.md
+++ src/main/java/net/eugenpaul/adventofcode/y2024/README.md
@@ -1,31 +1,31 @@
 # Solutions
 
-## Progress: Year 2024 [32/50]
+## Progress: Year 2024 [34/50]
 
-| Day | Status | Link                                 |
-| --- | ------ | ------------------------------------ |
-| 1   | ⭐⭐   | [Day 1: Historian Hysteria](day1)    |
-| 2   | ⭐⭐   | [Day 2: Red-Nosed Reports](day2)     |
-| 3   | ⭐⭐   | [Day 3: Mull It Over](day3)          |
-| 4   | ⭐⭐   | [Day 4: Ceres Search](day4)          |
-| 5   | ⭐⭐   | [Day 5: Print Queue](day5)           |
-| 6   | ⭐⭐   | [Day 6: Guard Gallivant](day6)       |
-| 7   | ⭐⭐   | [Day 7: Bridge Repair](day7)         |
-| 8   | ⭐⭐   | [Day 8: Resonant Collinearity](day8) |
-| 9   | ⭐⭐   | [Day 9: Disk Fragmenter](day9)       |
-| 10  | ⭐⭐   | [Day 10: Hoof It](day10)             |
-| 11  | ⭐⭐   | [Day 11: Plutonian Pebbles](day11)   |
-| 12  | ⭐⭐   | [Day 12: Garden Groups](day12)       |
-| 13  | ⭐⭐   | [Day 13: Claw Contraption](day13)    |
-| 14  | ⭐⭐   | [Day 14: Restroom Redoubt](day14)    |
-| 15  | ⭐⭐   | [Day 15: Warehouse Woes](day15)      |
-| 16  | ⭐⭐   | [Day 16: Reindeer Maze](day16)       |
-| 17  |        |                                      |
-| 18  |        |                                      |
-| 19  |        |                                      |
-| 20  |        |                                      |
-| 21  |        |                                      |
-| 22  |        |                                      |
-| 23  |        |                                      |
-| 24  |        |                                      |
-| 25  |        |                                      |
+| Day | Status | Link                                    |
+| --- | ------ | --------------------------------------- |
+| 1   | ⭐⭐   | [Day 1: Historian Hysteria](day1)       |
+| 2   | ⭐⭐   | [Day 2: Red-Nosed Reports](day2)        |
+| 3   | ⭐⭐   | [Day 3: Mull It Over](day3)             |
+| 4   | ⭐⭐   | [Day 4: Ceres Search](day4)             |
+| 5   | ⭐⭐   | [Day 5: Print Queue](day5)              |
+| 6   | ⭐⭐   | [Day 6: Guard Gallivant](day6)          |
+| 7   | ⭐⭐   | [Day 7: Bridge Repair](day7)            |
+| 8   | ⭐⭐   | [Day 8: Resonant Collinearity](day8)    |
+| 9   | ⭐⭐   | [Day 9: Disk Fragmenter](day9)          |
+| 10  | ⭐⭐   | [Day 10: Hoof It](day10)                |
+| 11  | ⭐⭐   | [Day 11: Plutonian Pebbles](day11)      |
+| 12  | ⭐⭐   | [Day 12: Garden Groups](day12)          |
+| 13  | ⭐⭐   | [Day 13: Claw Contraption](day13)       |
+| 14  | ⭐⭐   | [Day 14: Restroom Redoubt](day14)       |
+| 15  | ⭐⭐   | [Day 15: Warehouse Woes](day15)         |
+| 16  | ⭐⭐   | [Day 16: Reindeer Maze](day16)          |
+| 17  | ⭐⭐   | [Day 17: Chronospatial Computer](day17) |
+| 18  |        |                                         |
+| 19  |        |                                         |
+| 20  |        |                                         |
+| 21  |        |                                         |
+| 22  |        |                                         |
+| 23  |        |                                         |
+| 24  |        |                                         |
+| 25  |        |                                         |
---
+++ src/main/java/net/eugenpaul/adventofcode/y2024/day17/Day17.java
@@ -0,0 +1,190 @@
+package net.eugenpaul.adventofcode.y2024.day17;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+
+import lombok.Getter;
+import net.eugenpaul.adventofcode.helper.SolutionTemplate;
+import net.eugenpaul.adventofcode.helper.StringConverter;
+
+public class Day17 extends SolutionTemplate {
+
+    @Getter
+    private String totalScore;
+    @Getter
+    private long totalScore2;
+
+    public static void main(String[] args) {
+        Day17 puzzle = new Day17();
+        puzzle.doPuzzleFromFile("y2024/day17/puzzle1.txt");
+    }
+
+    @Override
+    public boolean doEvent(String eventData) {
+        return doEvent(List.of(eventData));
+    }
+
+    @Override
+    public boolean doEvent(List<String> eventData) {
+        totalScore = doPuzzle1(eventData);
+        totalScore2 = doPuzzle2(eventData);
+        return true;
+    }
+
+    public String doPuzzle1(List<String> eventData) {
+        String response;
+
+        long a = Long.parseLong(eventData.get(0).split(" ")[2]);
+        long b = Long.parseLong(eventData.get(1).split(" ")[2]);
+        long c = Long.parseLong(eventData.get(2).split(" ")[2]);
+
+        List<Integer> prog = StringConverter.toIntegerArrayList(eventData.get(4).split(" ")[1]);
+
+        List<String> output = new LinkedList<>();
+
+        for (int i = 0; i < prog.size(); i = i + 2) {
+            int opcode = prog.get(i);
+            int operand = prog.get(i + 1);
+            long operandValue;
+            switch (opcode) {
+            case 0:
+                operandValue = getComboValue(operand, a, b, c);
+                long tmp0 = 1 << operandValue;
+                a = a / tmp0;
+                break;
+            case 1:
+                b = b ^ operand;
+                break;
+            case 2:
+                b = getComboValue(operand, a, b, c) & 0x7;
+                break;
+            case 3:
+                if (a != 0) {
+                    i = operand - 2;
+                }
+                break;
+            case 4:
+                b = b ^ c;
+                break;
+            case 5:
+                long tt = getComboValue(operand, a, b, c) % 8;
+                output.add(tt + "");
+                break;
+            case 6:
+                operandValue = getComboValue(operand, a, b, c);
+                long tmp6 = 1 << operandValue;
+                b = a / tmp6;
+                break;
+            case 7:
+                operandValue = getComboValue(operand, a, b, c);
+                long tmp7 = 1 << operandValue;
+                c = a / tmp7;
+                break;
+            default:
+                throw new IllegalArgumentException();
+            }
+        }
+
+        response = String.join(",", output);
+
+        logger.log(Level.INFO, "Solution 1 " + response);
+        return response;
+    }
+
+    private long getComboValue(int operand, long a, long b, long c) {
+        switch (operand) {
+        case 0, 1, 2, 3:
+            return operand;
+        case 4:
+            return a;
+        case 5:
+            return b;
+        case 6:
+            return c;
+        default:
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Long doPuzzle2(List<String> eventData) {
+        long response = 0;
+
+        long b = Long.parseLong(eventData.get(1).split(" ")[2]);
+        long c = Long.parseLong(eventData.get(2).split(" ")[2]);
+
+        List<Integer> prog = StringConverter.toIntegerArrayList(eventData.get(4).split(" ")[1]);
+
+        for (int len = 1; len <= prog.size(); len++) {
+            for (long aCheck = 0L; aCheck <= 1_000_000L; aCheck++) {
+                long a = aCheck + response * 8;
+                List<Integer> output = new LinkedList<>();
+                for (int i = prog.size() - len; i < prog.size(); i++) {
+                    output.add(prog.get(i));
+                }
+                boolean ok = true;
+                boolean done = false;
+
+                try {
+                    for (int i = 0; i < prog.size() && ok && !done; i = i + 2) {
+                        int opcode = prog.get(i);
+                        int operand = prog.get(i + 1);
+                        long operandValue;
+                        switch (opcode) {
+                        case 0:
+                            operandValue = getComboValue(operand, a, b, c);
+                            long tmp0 = 1 << operandValue;
+                            a = a / tmp0;
+                            break;
+                        case 1:
+                            b = b ^ operand;
+                            break;
+                        case 2:
+                            b = getComboValue(operand, a, b, c) & 0x7;
+                            break;
+                        case 3:
+                            if (a != 0) {
+                                i = operand - 2;
+                            }
+                            break;
+                        case 4:
+                            b = b ^ c;
+                            break;
+                        case 5:
+                            long tt = getComboValue(operand, a, b, c) % 8;
+                            if (output.getFirst() != tt || output.isEmpty()) {
+                                ok = false;
+                            }
+                            output.removeFirst();
+                            done = output.isEmpty();
+                            break;
+                        case 6:
+                            operandValue = getComboValue(operand, a, b, c);
+                            long tmp6 = 1 << operandValue;
+                            b = a / tmp6;
+                            break;
+                        case 7:
+                            operandValue = getComboValue(operand, a, b, c);
+                            long tmp7 = 1 << operandValue;
+                            c = a / tmp7;
+                            break;
+                        default:
+                            throw new IllegalArgumentException();
+                        }
+                    }
+                } catch (Exception e) {
+                    throw new IllegalArgumentException();
+                }
+
+                if (done && ok) {
+                    response = response * 8 + aCheck;
+                    break;
+                }
+            }
+        }
+
+        logger.log(Level.INFO, "Solution 2 " + response);
+        return response;
+    }
+
+}
