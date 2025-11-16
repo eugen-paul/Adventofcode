cc577786a0a229181949059122e5536cb5ef99d0
Eugen Paul <paul.eugen@gmx.de>
2025-11-16 12:34:26 +0100

    Refactoring + lib update

--- src/main/java/net/eugenpaul/adventofcode/helper/JacksonHelper.java
+++ src/main/java/net/eugenpaul/adventofcode/helper/JacksonHelper.java
@@ -1,11 +1,10 @@
 package net.eugenpaul.adventofcode.helper;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.core.JacksonException;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class JacksonHelper {
@@ -15,7 +14,7 @@ public final class JacksonHelper {
     public static JsonNode toTree(String data) {
         try {
             return mapper.readTree(data);
-        } catch (JsonProcessingException e) {
+        } catch (JacksonException e) {
             throw new IllegalArgumentException("can't read the tree.", e);
         }
     }
