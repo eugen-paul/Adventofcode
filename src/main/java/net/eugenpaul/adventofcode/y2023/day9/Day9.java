af9d556d6bb2ec8130aace498d402f24877d78a4
Eugen Paul <paul.eugen@gmx.de>
2025-11-17 18:42:03 +0100

    Refactoring / Retrying 2023.09

--- src/main/java/net/eugenpaul/adventofcode/y2023/day9/Day9.java
+++ src/main/java/net/eugenpaul/adventofcode/y2023/day9/Day9.java
@@ -1,7 +1,9 @@
 package net.eugenpaul.adventofcode.y2023.day9;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import lombok.Getter;
 import net.eugenpaul.adventofcode.helper.SolutionTemplate;
@@ -30,6 +32,30 @@ public class Day9 extends SolutionTemplate {
         long response = 0;
 
         for (String data : eventData) {
+            response += getLast1(StringConverter.toLongArrayList(data));
+        }
+
+        logger.info("Solution 1 " + response);
+        return response;
+    }
+
+    private long getLast1(List<Long> in) {
+        Set<Long> diffs = new HashSet<>(in);
+        if (diffs.size() == 1 && diffs.contains(0L)) {
+            return 0;
+        }
+        List<Long> nextIn = new ArrayList<>();
+        for (int i = 1; i < in.size(); i++) {
+            nextIn.add(in.get(i) - in.get(i - 1));
+        }
+        long lastN = getLast1(nextIn);
+        return in.getLast() + lastN;
+    }
+
+    public long doPuzzle1_a(List<String> eventData) {
+        long response = 0;
+
+        for (String data : eventData) {
             response += getLast(StringConverter.toLongArrayList(data));
         }
 
@@ -38,6 +64,30 @@ public class Day9 extends SolutionTemplate {
     }
 
     public long doPuzzle2(List<String> eventData) {
+        long response = 0;
+
+        for (String data : eventData) {
+            response += getFirst1(StringConverter.toLongArrayList(data));
+        }
+
+        logger.info("Solution 2 " + response);
+        return response;
+    }
+
+    private long getFirst1(List<Long> in) {
+        Set<Long> diffs = new HashSet<>(in);
+        if (diffs.size() == 1 && diffs.contains(0L)) {
+            return 0;
+        }
+        List<Long> nextIn = new ArrayList<>();
+        for (int i = 1; i < in.size(); i++) {
+            nextIn.add(in.get(i) - in.get(i - 1));
+        }
+        long firstN = getFirst1(nextIn);
+        return in.getFirst() - firstN;
+    }
+
+    public long doPuzzle2a(List<String> eventData) {
         long response = 0;
 
         for (String data : eventData) {
