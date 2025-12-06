41742a5c1c6a0392702364dede56d86b45911241
Eugen Paul <paul.eugen@gmx.de>
2025-12-06 09:44:35 +0100

    Advent of Code 2025 Day 6

--- src/main/java/net/eugenpaul/adventofcode/y2025/day6/Day6.java
+++ src/main/java/net/eugenpaul/adventofcode/y2025/day6/Day6.java
@@ -41,6 +41,36 @@ public class Day6 extends SolutionTemplate {
     public long doPuzzle1(List<String> eventData) {
         long response = 0;
 
+        String ops = eventData.get(eventData.size() - 1).replace(" ", "");
+
+        List<List<Long>> z = new ArrayList<>();
+        for (int i = 0; i < eventData.size() - 1; i++) {
+            z.add(StringConverter.toLongArrayList(eventData.get(i)));
+        }
+
+        for (int i = 0; i < ops.length(); i++) {
+            if (ops.charAt(i) == '+') {
+                var res = 0L;
+                for (var zz : z) {
+                    res += zz.get(i);
+                }
+                response += res;
+            } else {
+                var res = 1L;
+                for (var zz : z) {
+                    res *= zz.get(i);
+                }
+                response += res;
+            }
+        }
+
+        logger.log(Level.INFO, "Solution 1 " + response);
+        return response;
+    }
+
+    public long doPuzzle1_a(List<String> eventData) {
+        long response = 0;
+
         String ll = eventData.get(eventData.size() - 1);
         for (int i = 0; i < 20; i++) {
             ll = ll.replace("  ", " ");
@@ -74,6 +104,45 @@ public class Day6 extends SolutionTemplate {
     }
 
     public long doPuzzle2(List<String> eventData) {
+        long response = 0;
+
+        String cur = "";
+        long buffer = 0L;
+        boolean mult = false;
+        for (int col = 0; col < eventData.get(0).length(); col++) {
+            cur = "";
+            for (int row = 0; row < eventData.size(); row++) {
+                var c = eventData.get(row).charAt(col);
+                if (Character.isDigit(c)) {
+                    cur += c;
+                } else if (c == '+') {
+                    mult = false;
+                    buffer = 0;
+                } else if (c == '*') {
+                    mult = true;
+                    buffer = 1;
+                }
+            }
+
+            if (cur.isBlank()) {
+                response += buffer;
+                continue;
+            }
+
+            if (mult) {
+                buffer *= Long.parseLong(cur.trim());
+            } else {
+                buffer += Long.parseLong(cur.trim());
+            }
+        }
+
+        response += buffer;
+
+        logger.log(Level.INFO, "Solution 2 " + response);
+        return response;
+    }
+
+    public long doPuzzle2_b(List<String> eventData) {
         long response = 0;
 
         String ll = eventData.get(eventData.size() - 1);
