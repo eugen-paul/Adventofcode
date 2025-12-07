caf99845dce38224960a69bddbe4e744437b2a44
Eugen Paul <paul.eugen@gmx.de>
2025-12-07 08:32:19 +0100

    Advent of Code 2025 Day 7

--- src/main/java/net/eugenpaul/adventofcode/y2025/day7/Day7.java
+++ src/main/java/net/eugenpaul/adventofcode/y2025/day7/Day7.java
@@ -72,36 +72,6 @@ public class Day7 extends SolutionTemplate {
         return response;
     }
 
-    public long doPuzzle2(List<String> eventData) {
-        long response = 0;
-
-        long[][] dp = new long[eventData.size()][eventData.get(0).length()];
-        for (var row : dp) {
-            Arrays.fill(row, 0);
-        }
-
-        int maxX = eventData.get(0).length() - 1;
-        int maxY = eventData.size() - 1;
-
-        Arrays.fill(dp[maxY], 1);
-
-        for (int y = maxY - 1; y > 0; y--) {
-            for (int x = 0; x <= maxX; x++) {
-                var c = eventData.get(y).charAt(x);
-                if (c == '.') {
-                    dp[y][x] = dp[y + 1][x];
-                } else {
-                    dp[y][x] = dp[y + 1][x - 1] + dp[y + 1][x + 1];
-                }
-            }
-        }
-
-        response = Arrays.stream(dp[1]).max().orElseThrow();
-
-        logger.log(Level.INFO, "Solution 2 " + response);
-        return response;
-    }
-
     public long doPuzzle1_a(List<String> eventData) {
         long response = 0;
 
@@ -129,6 +99,68 @@ public class Day7 extends SolutionTemplate {
         }
 
         logger.log(Level.INFO, "Solution 1 " + response);
+        return response;
+    }
+    
+    public long doPuzzle2(List<String> eventData) {
+        long response = 0;
+
+        long[] cur = new long[eventData.get(0).length()];
+        Arrays.fill(cur, 0);
+
+        long[] last = new long[eventData.get(0).length()];
+        Arrays.fill(last, 1);
+
+        int maxX = eventData.get(0).length() - 1;
+        int maxY = eventData.size() - 1;
+
+        for (int y = maxY - 1; y > 0; y--) {
+            for (int x = 0; x <= maxX; x++) {
+                var c = eventData.get(y).charAt(x);
+                if (c == '.') {
+                    cur[x] = last[x];
+                } else {
+                    cur[x] = last[x - 1] + last[x + 1];
+                }
+            }
+            var tmp = cur;
+            cur = last;
+            last = tmp;
+        }
+
+        response = Arrays.stream(last).max().orElseThrow();
+
+        logger.log(Level.INFO, "Solution 2 " + response);
+        return response;
+    }
+
+    public long doPuzzle2_b(List<String> eventData) {
+        long response = 0;
+
+        long[][] dp = new long[eventData.size()][eventData.get(0).length()];
+        for (var row : dp) {
+            Arrays.fill(row, 0);
+        }
+
+        int maxX = eventData.get(0).length() - 1;
+        int maxY = eventData.size() - 1;
+
+        Arrays.fill(dp[maxY], 1);
+
+        for (int y = maxY - 1; y > 0; y--) {
+            for (int x = 0; x <= maxX; x++) {
+                var c = eventData.get(y).charAt(x);
+                if (c == '.') {
+                    dp[y][x] = dp[y + 1][x];
+                } else {
+                    dp[y][x] = dp[y + 1][x - 1] + dp[y + 1][x + 1];
+                }
+            }
+        }
+
+        response = Arrays.stream(dp[1]).max().orElseThrow();
+
+        logger.log(Level.INFO, "Solution 2 " + response);
         return response;
     }
 
